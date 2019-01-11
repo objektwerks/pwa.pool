4c57809e8b987c338f96ce40f5d90de9cca770eb
objektwerks <objektwerks@runbox.com>
2019-01-11 10:07:44 -0500

    added postOwner route and query

--- jvm/src/main/scala/tripletail/PoolRepository.scala
+++ jvm/src/main/scala/tripletail/PoolRepository.scala
@@ -1,13 +1,15 @@
 package tripletail
 
 import io.getquill._
-import scala.concurrent.{ExecutionContext, Future}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object PoolRepository {
   implicit val ctx = new PostgresAsyncContext(SnakeCase, "quill.ctx")
   import ctx._
 
-  def getOwner(license: String)(implicit ec: ExecutionContext): Future[Option[Owner]] = {
-    run( query[Owner].filter(_.license == lift(license)) ).map(_.headOption)
-  }
+  def postOwner(owner: Owner): Future[Unit] = run( query[Owner].insert(lift(owner)) ).mapTo[Unit]
+
+  def getOwner(license: String): Future[Option[Owner]] = run( query[Owner].filter(_.license == lift(license)) ).map(_.headOption)
 }
--- jvm/src/main/scala/tripletail/PoolService.scala
+++ jvm/src/main/scala/tripletail/PoolService.scala
@@ -1,32 +1,37 @@
 package tripletail
 
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.generic.auto._
 
-import scala.concurrent.ExecutionContext
-
 object PoolService {
-  implicit val ec = ExecutionContext.Implicits.global
-
   val index = path("") {
     getFromResource("public/index.html")
   }
   val resources = get {
     getFromResourceDirectory("public")
   }
+  val postOwner = path("owners") {
+    post {
+      entity(as[Owner]) { owner =>
+        onSuccess(PoolRepository.postOwner(owner)) {
+          complete( OK )
+        }
+      }
+    }
+  }
   val getOwner = path("owners" / Segment) { license =>
     get {
       onSuccess(PoolRepository.getOwner(license)) {
         case Some(owner) => complete( ToResponseMarshallable[Owner](owner) )
-        case None => complete( StatusCodes.NotFound )
+        case None => complete( NotFound )
       }
     }
   }
   val api = pathPrefix("api" / "v1" / "tripletail") {
-    getOwner
+    postOwner ~ getOwner
   }
-  val routes = index ~ resources
+  val routes = index ~ resources ~ api
 }
