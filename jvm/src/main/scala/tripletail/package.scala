ad4ebb15851d524fbc6afec3354d8c55df45e6ee
objektwerks <objektwerks@runbox.com>
2019-01-20 12:16:19 -0500

    added licensee cache

--- jvm/src/main/scala/tripletail/package.scala
+++ jvm/src/main/scala/tripletail/package.scala
@@ -1,17 +1,41 @@
+import java.util.concurrent.TimeUnit
+
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.github.benmanes.caffeine.cache.Caffeine
 import io.circe.generic.auto._
 import io.circe.parser.decode
+import scalacache.caffeine.CaffeineCache
+import scalacache.modes.sync._
+import scalacache.{Cache, Entry}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 package object tripletail {
-  private def isLicenseeSecure(licensee: Licensee): Future[Boolean] = Future.successful(licensee.license.nonEmpty)
+  val cacheConf = Caffeine.newBuilder.maximumSize(10000L).expireAfterWrite(24, TimeUnit.HOURS).build[String, Entry[Licensee]]
+  implicit val cache: Cache[Licensee] = CaffeineCache[Licensee](cacheConf)
+
+  def isLicenseeSecure(license: String): Future[Boolean] = {
+    if (cache.get(license).nonEmpty) {
+      Future.successful(true)
+    } else {
+      PoolStore.getLicensee(license).flatMap { option =>
+        if (option.nonEmpty) {
+          val licensee = option.get
+          cache.put(licensee.license)(licensee)
+          Future.successful(true)
+        } else {
+          Future.successful(false)
+        }
+      }
+    }
+  }
 
   def secure(route: Route): Route = headerValueByName("licensee") { json =>
     decode[Licensee](json) match {
-      case Right(licensee) => onSuccess(isLicenseeSecure(licensee)) { isSecure =>
+      case Right(licensee) => onSuccess(isLicenseeSecure(licensee.license)) { isSecure =>
         if (isSecure) route else complete(StatusCodes.Unauthorized)
       }
       case Left(error) => extractLog { log =>
