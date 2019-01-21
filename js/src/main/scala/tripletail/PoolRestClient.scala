04e6d2dffc8739b61a56ec31572212c9903db8e3
objektwerks <objektwerks@runbox.com>
2019-01-21 08:37:18 -0500

    added signup and signin calls to js rest client

--- js/src/main/scala/tripletail/PoolRestClient.scala
+++ js/src/main/scala/tripletail/PoolRestClient.scala
@@ -1,7 +1,35 @@
 package tripletail
 
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import org.scalajs.dom.ext.Ajax
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 class PoolRestClient(url: String) {
-  println(url)
+  val headers = Map("Content-Type" -> "application/json; charset=utf-8", "Accept" -> "application/json")
+
+  def signup(signup: SignUp): Future[Either[Fault, SignedUp]] = {
+    Ajax.post(url = url, headers = headers, data = signup.asJson.toString).map { xhr =>
+      xhr.status match {
+        case 200 => decode[SignedUp](xhr.responseText).fold(e => Left(Fault(e.getMessage)), v => Right(v))
+        case _ => Left(toFault(xhr.status))
+      }
+    }
+  }
+
+  def signin(signin: SignIn): Future[Either[Fault, SignedIn]] = {
+    Ajax.post(url = url, headers = headers, data = signin.asJson.toString).map { xhr =>
+      xhr.status match {
+        case 200 => decode[SignedIn](xhr.responseText).fold(e => Left(Fault(e.getMessage)), v => Right(v))
+        case _ => Left(toFault(xhr.status))
+      }
+    }
+  }
+
+  def toFault(statusCode: Int): Fault = Fault(s"status code: ${statusCode.toString}")
 }
 
 object PoolRestClient {
--- shared/src/main/scala/tripletail/Event.scala
+++ shared/src/main/scala/tripletail/Event.scala
@@ -2,6 +2,8 @@ package tripletail
 
 sealed trait Event extends Product with Serializable
 
+final case class Fault(message: String) extends Event
+
 final case class SignedUp(licensee: Licensee) extends Event
 
 final case class SignedIn(licensee: Licensee, pools: Seq[Pool]) extends Event
