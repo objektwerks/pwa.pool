9eb531fdf6919564a3f246baf751d65f068dd0bc
objektwerks <objektwerks@runbox.com>
2019-01-28 09:48:04 -0500

    added enhanced fault and console logging to rest client

--- js/src/main/scala/tripletail/PoolRestClient.scala
+++ js/src/main/scala/tripletail/PoolRestClient.scala
@@ -14,23 +14,31 @@ class PoolRestClient(url: String) {
   def post(path: String, command: Command): Future[Either[Fault, Event]] = {
     Ajax.post(url = url + path, headers = headers, data = command.asJson.toString).map { xhr =>
       xhr.status match {
-        case 200 => decode[Event](xhr.responseText).fold(e => Left(Fault(e.getMessage)), v => Right(v))
+        case 200 => decode[Event](xhr.responseText).fold(e => Left(toFault(e)), v => Right(v))
         case _ => Left(toFault(xhr.status))
       }
-    }.recover { case e => Left(Fault(e.getMessage)) }
+    }.recover { case e => Left(toFault(e)) }
   }
 
   def post(path: String, license: String, entity: Entity): Future[Either[Fault, State]] = {
     val headersWithLicense = headers ++: Map("license" -> license)
     Ajax.post(url = url + path, headers = headersWithLicense, data = entity.asJson.toString).map { xhr =>
       xhr.status match {
-        case 200 => decode[State](xhr.responseText).fold(e => Left(Fault(e.getMessage)), v => Right(v))
+        case 200 => decode[State](xhr.responseText).fold(e => Left(toFault(e)), v => Right(v))
         case _ => Left(toFault(xhr.status))
       }
-    }.recover { case e => Left(Fault(e.getMessage)) }
+    }.recover { case e => Left(toFault(e)) }
   }
 
-  def toFault(statusCode: Int): Fault = Fault(s"http status code: ${statusCode.toString}", statusCode)
+  def toFault(statusCode: Int): Fault = {
+    println(s"post http status code error: $statusCode")
+    Fault(s"http status code: ${statusCode.toString}", statusCode)
+  }
+
+  def toFault(t: Throwable): Fault = {
+    println(s"post method exception: ${t.printStackTrace()}")
+    Fault(s"${t.getMessage}")
+  }
 }
 
 object PoolRestClient {
