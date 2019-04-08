5f9fb338fd0e649c6c69c3eadce9255207837156
objektwerks <objektwerks@runbox.com>
2019-04-08 14:58:32 -0400

    removed Item

--- shared/src/main/scala/tripletail/State.scala
+++ shared/src/main/scala/tripletail/State.scala
@@ -4,8 +4,6 @@ sealed trait State extends Product with Serializable
 
 final case class Id(id: Int) extends State
 
-final case class Item(entity: Entity) extends State
-
 final case class Sequence(seq: Seq[Entity]) extends State
 
 case object Ok extends State
