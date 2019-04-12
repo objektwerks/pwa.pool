457f4abe325bb454e1e2a9ac19ad9bb4e64ecdce
objektwerks <objektwerks@runbox.com>
2019-04-12 16:40:59 -0400

    moved fault code into fault object

--- js/src/main/scala/tripletail/PoolRestClient.scala
+++ js/src/main/scala/tripletail/PoolRestClient.scala
@@ -14,30 +14,20 @@ class PoolRestClient(serverUrl: String) {
   def post(path: String, command: Command): Future[Either[Fault, Event]] = {
     Ajax.post(url = serverUrl + path, headers = headers, data = command.asJson.toString).map { xhr =>
       xhr.status match {
-        case 200 => decode[Event](xhr.responseText).fold(e => Left(toFault(e)), v => Right(v))
-        case _ => Left(toFault(xhr.status))
+        case 200 => decode[Event](xhr.responseText).fold(e => Left(Fault.toFault(e)), v => Right(v))
+        case _ => Left(Fault.toFault(xhr.status))
       }
-    }.recover { case e => Left(toFault(e)) }
+    }.recover { case e => Left(Fault.toFault(e)) }
   }
 
   def post(path: String, license: String, entity: Entity): Future[Either[Fault, State]] = {
     val headersWithLicense = headers ++: Map("license" -> license)
     Ajax.post(url = serverUrl + path, headers = headersWithLicense, data = entity.asJson.toString).map { xhr =>
       xhr.status match {
-        case 200 => decode[State](xhr.responseText).fold(e => Left(toFault(e)), v => Right(v))
-        case _ => Left(toFault(xhr.status))
+        case 200 => decode[State](xhr.responseText).fold(e => Left(Fault.toFault(e)), v => Right(v))
+        case _ => Left(Fault.toFault(xhr.status))
       }
-    }.recover { case e => Left(toFault(e)) }
-  }
-
-  def toFault(statusCode: Int): Fault = {
-    println(s"post http status code error: $statusCode")
-    Fault(s"http status code: ${statusCode.toString}", statusCode)
-  }
-
-  def toFault(t: Throwable): Fault = {
-    println(s"post method exception: ${t.printStackTrace()}")
-    Fault(s"${t.getMessage}")
+    }.recover { case e => Left(Fault.toFault(e)) }
   }
 }
 
--- shared/src/main/scala/tripletail/Fault.scala
+++ shared/src/main/scala/tripletail/Fault.scala
@@ -1,3 +1,15 @@
 package tripletail
 
 final case class Fault(message: String, code: Int = 0)
+
+object Fault {
+  def toFault(statusCode: Int): Fault = {
+    println(s"post http status code error: $statusCode")
+    Fault(s"http status code: ${statusCode.toString}", statusCode)
+  }
+
+  def toFault(t: Throwable): Fault = {
+    println(s"post method exception: ${t.printStackTrace()}")
+    Fault(s"${t.getMessage}")
+  }
+}
