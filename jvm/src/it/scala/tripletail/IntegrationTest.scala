7827e25e8e7a2bf9b31cb5cfc5284c3f87bdf14e
objektwerks <objektwerks@runbox.com>
2019-06-02 12:07:34 -0400

    added PoolApp.main(..) to IntegrationTest

--- jvm/src/it/scala/tripletail/IntegrationTest.scala
+++ jvm/src/it/scala/tripletail/IntegrationTest.scala
@@ -3,7 +3,8 @@ package tripletail
 import org.scalatest.{FunSuite, Matchers}
 
 class IntegrationTest extends FunSuite with Matchers {
+  PoolApp.main(Array.empty[String])
+
   test("integration") {
-    println("integration test...")
   }
 }
