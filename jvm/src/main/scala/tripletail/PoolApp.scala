dc1b9f75ecb7b1192561ef7f0f0189472814fab0
objektwerks <objektwerks@runbox.com>
2019-06-02 08:51:09 -0400

    added shutdown hook to PoolApp

--- jvm/src/main/scala/tripletail/PoolApp.scala
+++ jvm/src/main/scala/tripletail/PoolApp.scala
@@ -6,20 +6,28 @@ import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
-object PoolApp {
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
-    val conf = ConfigFactory.load("app.conf")
-    val store = PoolStore(conf)(system.dispatcher)
-    val cache = LicenseeCache(store)
-    val routes = PoolRoutes(store, cache)
-    val host = conf.getString("app.host")
-    val port = conf.getInt("app.port")
-    Http().bindAndHandle(routes.routes, host, port)
+object PoolApp extends App {
+  val logger = LoggerFactory.getLogger(PoolApp.getClass)
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
 
-    val logger = LoggerFactory.getLogger(PoolApp.getClass)
-    logger.info(s"Pool app started at http://$host:$port")
+  val conf = ConfigFactory.load("app.conf")
+  val store = PoolStore(conf)(system.dispatcher)
+  val cache = LicenseeCache(store)
+  val routes = PoolRoutes(store, cache)
+  val host = conf.getString("app.host")
+  val port = conf.getInt("app.port")
+  Http().bindAndHandle(routes.routes, host, port)
+
+  logger.info(s"Pool app started at http://$host:$port")
+
+  sys.addShutdownHook {
+    logger.info("Pool app shutting down...")
+    system.terminate()
+    Await.result(system.whenTerminated, 30.seconds)
+    logger.info("Pool app shutdown.")
   }
 }
