c043cbf2a2b442d9a3952a5325a863bf6fe2e601
objektwerks <objektwerks@runbox.com>
2019-06-04 14:56:31 -0400

    removed sttp dependency, readded akka-http-testkit dependency

--- jvm/src/it/scala/tripletail/IntegrationTest.scala
+++ jvm/src/it/scala/tripletail/IntegrationTest.scala
@@ -3,8 +3,9 @@ package tripletail
 import org.scalatest.{FunSuite, Matchers}
 
 class IntegrationTest extends FunSuite with Matchers {
-  PoolApp.main(Array.empty[String])
+  val url = "http://127.0.0.1:7979/api/v1/tripletail/"
+  val headers = Map("Content-Type" -> "application/json; charset=utf-8", "Accept" -> "application/json")
 
-  test("integration") {
+  test("fault") {
   }
 }
