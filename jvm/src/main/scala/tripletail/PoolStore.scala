88ed6698ff1b234b5b762a8bd9443d4bc8d36e80
objektwerks <objektwerks@runbox.com>
2019-06-05 17:22:36 -0400

    refactored pool store signup method

--- jvm/src/main/scala/tripletail/PoolStore.scala
+++ jvm/src/main/scala/tripletail/PoolStore.scala
@@ -16,9 +16,8 @@ class PoolStore(conf: Config)(implicit ec: ExecutionContext) {
   def signUp(email: String): Future[Licensee] = {
     val licensee = Licensee(email = email)
     ctx.transaction { implicit ec =>
-      run(query[Licensee].insert(lift(licensee)))
+      run(query[Licensee].insert(lift(licensee))).map(_ => licensee)
     }
-    Future.successful(licensee)
   }
 
   def signIn(license: String, email: String): Future[Option[Licensee]] = {
