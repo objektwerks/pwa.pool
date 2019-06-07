1e048fb8e19e8956dcf2d330e1dd8d3b7935cc6b
objektwerks <objektwerks@runbox.com>
2019-06-06 20:01:36 -0400

    edited fault code default value to 500

--- shared/src/main/scala/tripletail/Fault.scala
+++ shared/src/main/scala/tripletail/Fault.scala
@@ -2,7 +2,7 @@ package tripletail
 
 import java.time.Instant
 
-final case class Fault(message: String, code: Int = 0, occurred: Long = Instant.now.toEpochMilli)
+final case class Fault(message: String, code: Int = 500, occurred: Long = Instant.now.toEpochMilli)
 
 object Fault {
   def apply(statusText: String, statusCode: Int): Fault = Fault(statusText, statusCode)
