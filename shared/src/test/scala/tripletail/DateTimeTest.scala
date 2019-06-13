5043fd33a4dc81b284d138bd20fbc5eb6f64fe19
objektwerks <objektwerks@runbox.com>
2019-06-13 13:55:23 -0400

    implemented DateTimeTest

--- shared/src/test/scala/tripletail/DateTimeTest.scala
+++ shared/src/test/scala/tripletail/DateTimeTest.scala
@@ -1,13 +1,28 @@
 package tripletail
 
+import java.time.{LocalDate, LocalTime}
+
 import org.scalatest.{FunSuite, Matchers}
+import tripletail.DateTime._
 
 class DateTimeTest extends FunSuite with Matchers {
   test("date") {
+    val localDate = LocalDate.of(1999, 3, 13)
 
+    localDateToString(localDate) shouldEqual "1999-03-13"
+    localDateToInt(localDate) shouldEqual 19990313
+    localDateToInt(1999, 3, 13) shouldEqual 19990313
+    localDateAsStringToInt(localDateToString(localDate)) shouldEqual 19990313
+    localDateAsIntToString(localDateToInt(localDate)) shouldEqual "1999-03-13"
   }
 
   test("time") {
+    val localTime = LocalTime.of(3, 33)
 
+    localTimeToString(localTime) shouldEqual "03:33"
+    localTimeToInt(localTime) shouldEqual 333
+    localTimeToInt(3, 33) shouldEqual 333
+    localTimeAsStringToInt(localTimeToString(localTime)) shouldEqual 333
+    localTimeAsIntToString(localTimeToInt(localTime)) shouldEqual "03:33"
   }
 }
