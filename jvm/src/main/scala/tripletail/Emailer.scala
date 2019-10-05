ca2a2a2f6767d3bbdc23370ae954cc2e243db47f
objektwerks <objektwerks@runbox.com>
2019-10-05 16:10:43 -0400

    implemented emailer send email with retry strategy

--- jvm/src/main/scala/tripletail/Emailer.scala
+++ jvm/src/main/scala/tripletail/Emailer.scala
@@ -2,7 +2,7 @@ package tripletail
 
 import akka.actor.{Actor, ActorLogging}
 import com.typesafe.config.Config
-import jodd.mail.{Email, MailServer, SmtpServer}
+import jodd.mail.{Email, MailServer, SendMailSession, SmtpServer}
 
 class Emailer(conf: Config) extends Actor with ActorLogging {
   private val smtpServer: SmtpServer = MailServer.create()
@@ -14,6 +14,7 @@ class Emailer(conf: Config) extends Actor with ActorLogging {
   private val from = conf.getString("email.from")
   private val subject = conf.getString("email.subject")
   private val message = conf.getString("email.message")
+  private val retries = conf.getInt("email.retries")
 
   private def buildEmail(to: String, license: String): Email = Email.create()
     .from(from)
@@ -21,15 +22,28 @@ class Emailer(conf: Config) extends Actor with ActorLogging {
     .subject(subject)
     .textMessage(s"$message: $license")
 
-  private def sendEmail(to: String, license: String): Unit = {
-    val email: Email = buildEmail(to, license)
-    val session = smtpServer.createSession
-    session.open()
-    session.sendMail(email)
-    session.close()
+  private def sendEmail(to: String, license: String): Option[String] = {
+    var session: SendMailSession = null
+    var messageId: Option[String] = None
+    try {
+      session = smtpServer.createSession
+      session.open()
+      messageId = Some( session.sendMail(buildEmail(to, license)) )
+    } catch {
+      case t: Throwable => log.error(s"*** Emailer send to: $to failed: ${t.getMessage}")
+    } finally {
+      session.close()
+    }
+    messageId
   }
 
   override def receive: Receive = {
-    case send: SendEmail => sendEmail(send.to, send.license)
+    case send: SendEmail =>
+      var attempts = 0
+      var messageId: Option[String] = None
+      while ( attempts < retries && messageId.isEmpty ) {
+        messageId = sendEmail(send.to, send.license)
+        attempts = attempts + 1
+      }
   }
 }
