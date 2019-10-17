a489a84cf35240f6f4b03ed6d78776b748638833
objektwerks <objektwerks@runbox.com>
2019-10-17 05:25:21 -0400

    added emailer html conf

--- jvm/src/main/scala/tripletail/Emailer.scala
+++ jvm/src/main/scala/tripletail/Emailer.scala
@@ -16,6 +16,9 @@ class Emailer(conf: Config) extends Actor with ActorLogging {
   private val from = conf.getString("email.from")
   private val subject = conf.getString("email.subject")
   private val message = conf.getString("email.message")
+  private val email = conf.getString("email.email")
+  private val lic = conf.getString("email.lic")
+  private val activating = conf.getString("email.activating")
   private val activated = conf.getString("email.activated")
   private val activationFailed = conf.getString("email.activationFailed")
   private val retries = conf.getInt("email.retries")
@@ -31,8 +34,8 @@ class Emailer(conf: Config) extends Actor with ActorLogging {
                   |<body>
                   |<h3>$subject</h3>
                   |<div>
-                  |<p>Email: $to</p>
-                  |<p>License: $license</p>
+                  |<p>$email $to</p>
+                  |<p>$lic $license</p>
                   |<p id="onActivated"></p>
                   |<p>$message<button type="button" onclick="activateLicense()">$subject</button></p>
                   |</div>
@@ -40,7 +43,7 @@ class Emailer(conf: Config) extends Actor with ActorLogging {
                   |function activateLicense() {
                   |  var xhr = new XMLHttpRequest();
                   |  xhr.onreadystatechange = function() {
-                  |    document.getElementById("onActivated").innerHTML = $activationFailed
+                  |    document.getElementById("onActivated").innerHTML = $activating
                   |    if (this.readyState == 4 && this.status == 200) {
                   |      document.getElementById("onActivated").innerHTML = $activated + " on " + xhr.responseText;
                   |    } else if (this.readyState == 4 && this.status != 200) {
