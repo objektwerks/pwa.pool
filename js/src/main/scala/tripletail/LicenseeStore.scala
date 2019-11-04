172e7632c086b09a729d43b2b945cf84923cec74
objektwerks <objektwerks@runbox.com>
2019-11-04 13:53:32 -0500

    renamed IndexedStore to LicenseeStore with embedded LicenseeCache

--- js/src/main/scala/tripletail/LicenseeStore.scala
+++ js/src/main/scala/tripletail/LicenseeStore.scala
@@ -1,7 +1,13 @@
 package tripletail
 
-object IndexedStore {
+import org.scalajs.dom._
+
+object LicenseeStore {
+  private val dbName = "licensee"
+  private val dbVersion = 1
   private var licenseeCache: Option[Licensee] = None
+
+  val openRequest = window.indexedDB.open(dbName, dbVersion)
 
   def getLicensee: Licensee = licenseeCache.getOrElse(putLicensee(Licensee(emailAddress = "tripletail@gmail.com")))
 
