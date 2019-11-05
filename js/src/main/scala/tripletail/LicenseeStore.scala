0885a1576a0b08f83634eda6bd2488e7305e2f6a
objektwerks <objektwerks@runbox.com>
2019-11-05 13:53:08 -0500

    refactored store key path and key

--- js/src/main/scala/tripletail/LicenseeStore.scala
+++ js/src/main/scala/tripletail/LicenseeStore.scala
@@ -9,20 +9,21 @@ object LicenseeStore {
   private val dbName = "db"
   private val dbVersion = 1
 
+  private val storeKeyPath = "{ keyPath: 'key' }"
+  private val storeKey = 1
+
   private val cryptoStore = "crypto"
-  private val cryptoStoreKey = "key"
   private var cryptoKeyCache: Option[CryptoKey] = None
 
   private val licenseeStore = "licensee"
-  private val licenseeStoreKey = "key"
   private var licenseeCache: Option[Licensee] = None
 
   private val openDBRequest = window.indexedDB.open(dbName, dbVersion)
 
   openDBRequest.onupgradeneeded = (event: IDBVersionChangeEvent) => {
     val db = openDBRequest.result.asInstanceOf[IDBDatabase]
-    db.createObjectStore(cryptoStore, s"{ keyPath: $cryptoStoreKey }")
-    db.createObjectStore(licenseeStore, s"{ keyPath: $licenseeStoreKey }")
+    db.createObjectStore(cryptoStore, storeKeyPath)
+    db.createObjectStore(licenseeStore, storeKeyPath)
     // Create CryptoKey and put in store.
     console.log("openDBRequest.onupgradeneeded", event)
   }
@@ -38,7 +39,7 @@ object LicenseeStore {
 
   private def cacheCryptoKey(db: IDBDatabase): Unit = {
     val store = db.transaction(cryptoStore, "readonly").objectStore(cryptoStore)
-    val dbRequest = store.get(cryptoStoreKey)
+    val dbRequest = store.get(storeKey)
     dbRequest.onerror = (event: ErrorEvent) => console.error("cacheCryptoKey", event)
     dbRequest.onsuccess = (event: dom.Event) => {
       val cryptoKey = dbRequest.result.asInstanceOf[CryptoKey]
@@ -50,7 +51,7 @@ object LicenseeStore {
   private def cacheLicensee(db: IDBDatabase): Unit = {
     val db = openDBRequest.result.asInstanceOf[IDBDatabase]
     val store = db.transaction(licenseeStore, "readonly").objectStore(licenseeStore)
-    val dbRequest = store.get(licenseeStoreKey)
+    val dbRequest = store.get(storeKey)
     dbRequest.onerror = (event: ErrorEvent) => console.error("cacheLicensee", event)
     dbRequest.onsuccess = (event: dom.Event) => {
       val licensee = dbRequest.result.asInstanceOf[Licensee]
