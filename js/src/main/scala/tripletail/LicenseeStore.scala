81e516919bedf451dec348ef02137dc9ac020ea1
objektwerks <objektwerks@runbox.com>
2019-11-05 15:44:14 -0500

    added putLicensee; added uPickle write and read for putLicensee and cacheLicensee

--- js/src/main/scala/tripletail/LicenseeStore.scala
+++ js/src/main/scala/tripletail/LicenseeStore.scala
@@ -53,26 +53,42 @@ object LicenseeStore {
     store.put(cryptoKey, storeKey)
   }
 
+  def putLicense(db: IDBDatabase, licensee: Licensee): Unit = {
+    import Serializers._
+    import upickle.default._
+
+    val db = openDBRequest.result.asInstanceOf[IDBDatabase]
+    val store = db.transaction(licenseeStore, "readwrite").objectStore(licenseeStore)
+    store.put(write(licensee), storeKey)
+  }
+
   private def cacheCryptoKey(db: IDBDatabase): Unit = {
     val store = db.transaction(cryptoStore, "readonly").objectStore(cryptoStore)
     val dbRequest = store.get(storeKey)
-    dbRequest.onerror = (event: ErrorEvent) => console.error("cacheCryptoKey", event)
+    dbRequest.onerror = (event: ErrorEvent) => console.error("cacheCryptoKey.onerror", event)
     dbRequest.onsuccess = (event: dom.Event) => {
-      val cryptoKey = dbRequest.result.asInstanceOf[CryptoKey]
-      cryptoKeyCache = Some(cryptoKey)
-      console.log("cacheCryptoKey: db request => on success", event)
+      if (!js.isUndefined(dbRequest.result)) {
+        val cryptoKey = dbRequest.result.asInstanceOf[CryptoKey]
+        cryptoKeyCache = Some(cryptoKey)
+        console.log("cacheCryptoKey.onsuccess", event)
+      } else console.error("cacheCryptoKey: db request => on error", event)
     }
   }
 
   private def cacheLicensee(db: IDBDatabase): Unit = {
+    import Serializers._
+    import upickle.default._
+
     val db = openDBRequest.result.asInstanceOf[IDBDatabase]
     val store = db.transaction(licenseeStore, "readonly").objectStore(licenseeStore)
     val dbRequest = store.get(storeKey)
-    dbRequest.onerror = (event: ErrorEvent) => console.error("cacheLicensee", event)
+    dbRequest.onerror = (event: ErrorEvent) => console.error("cacheLicensee.onerror", event)
     dbRequest.onsuccess = (event: dom.Event) => {
-      val licensee = dbRequest.result.asInstanceOf[Licensee]
-      licenseeCache = Some(licensee)
-      console.log("cacheLicensee: db request => on success", event)
+      if (!js.isUndefined(dbRequest.result)) {
+        val licensee = dbRequest.result.asInstanceOf[String]
+        licenseeCache = Some(read(licensee))
+        console.log("cacheLicensee.onsuccess", event)
+      } else console.log("cacheLicensee.onerror", event)
     }
   }
 }
