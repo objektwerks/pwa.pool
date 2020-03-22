e0150b3ae83eb9f87641c1921c83a54fd992b061
objektwerks <objektwerks@runbox.com>
2020-03-22 17:04:54 -0400

    edited integration test

--- jvm/src/it/scala/pool/IntegrationTest.scala
+++ jvm/src/it/scala/pool/IntegrationTest.scala
@@ -1,6 +1,5 @@
 package pool
 
-import de.heikoseeberger.akkahttpupickle.{UpickleSupport => Upickle}
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
@@ -8,7 +7,6 @@ import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import akka.testkit.TestDuration
 import com.typesafe.config.ConfigFactory
-import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.slf4j.LoggerFactory
@@ -16,7 +14,7 @@ import org.slf4j.LoggerFactory
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-class IntegrationTest extends AnyWordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
+class IntegrationTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
   val logger = LoggerFactory.getLogger(getClass)
   val conf = ConfigFactory.load("test.server.conf")
   val actorRefFactory = ActorSystem.create(conf.getString("server.name"), conf.getConfig("akka"))
@@ -35,6 +33,7 @@ class IntegrationTest extends AnyWordSpec with Matchers with ScalaFutures with S
       logger.info(s"*** Server integration test host: ${server.localAddress.toString}")
     }
 
+  import de.heikoseeberger.akkahttpupickle.{UpickleSupport => Upickle}
   import Upickle._
   import DateTime._
   import StatusCodes._
