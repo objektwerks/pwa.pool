fcaa298276947d58331ed1157983a9d018b8709d
objektwerks <objektwerks@runbox.com>
2021-05-07 07:44:42 -0400

    refactored container to invoke server proxy get

--- js/src/main/scala/pool/Container.scala
+++ js/src/main/scala/pool/Container.scala
@@ -2,10 +2,14 @@ package pool
 
 import com.raquo.laminar.api.L._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Container {
   def apply(serverProxy: ServerProxy, apiUrl: String): HtmlElement = {
-    println(serverProxy)
-    println(apiUrl)
-    div( p( "Todo!") )
+    val datetimeVar = Var("")
+    serverProxy.get(s"$apiUrl/now").foreach( now => datetimeVar.set(now) )
+    div(
+      child.text <-- datetimeVar
+    )
   }
 }
