089e1b9387c02f0ef1dbf1870cda47d0867f1bba
objektwerks <objektwerks@runbox.com>
2021-05-11 10:56:53 -0400

    edited index.html and container

--- js/src/main/scala/pool/Container.scala
+++ js/src/main/scala/pool/Container.scala
@@ -7,16 +7,25 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object Container {
   def apply(serverProxy: ServerProxy, publicUrl: String, apiUrl: String): HtmlElement = {
     println(s"public url: $publicUrl")
-    println(s"apil url: $apiUrl")
+    println(s"api url: $apiUrl")
     println(s"now url: $publicUrl/now")
 
+    div(
+      cls("w3-container w3-white"),
+      renderNow(serverProxy, publicUrl)
+    )
+  }
+
+  def renderNow(serverProxy: ServerProxy, publicUrl: String): Div = {
     val datetimeVar = Var("")
     serverProxy.get(s"$publicUrl/now").foreach( now => datetimeVar.set(now.stripPrefix("\"").stripSuffix("\"")) )
     div(
       cls("w3-container w3-white"),
-      fontSize("12px"),
-      color("indigo"),
-      child.text <-- datetimeVar
+      label(
+        fontSize("11px"),
+        color("indigo"),
+        child.text <-- datetimeVar
+      )
     )
   }
 }
