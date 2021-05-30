78186db6b4aff69d6f046de0f0d7c85ebf1eaf92
objektwerks <objektwerks@runbox.com>
2021-05-30 08:10:21 -0400

    refactoted int input

--- js/src/main/scala/pool/Client.scala
+++ js/src/main/scala/pool/Client.scala
@@ -96,7 +96,7 @@ class Client(publicUrl: String, apiUrl: String) extends js.Object {
         div(cls("w3-col"), width("85%"),
           input( idAttr("login-pin"), cls("w3-input w3-hover-light-gray w3-text-indigo"), typ("text"),
             inContext { input =>
-              onKeyUp.mapTo(input.ref.value).filter(_.nonEmpty).filter(_.toIntOption.nonEmpty) --> { value =>
+              onKeyUp.mapTo(input.ref.value).filter(_.toIntOption.nonEmpty) --> { value =>
                 pin.set(value.toInt)
               }
             }
