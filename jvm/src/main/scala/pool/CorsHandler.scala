584ca9cb44b67d88d2dc59a9794a43cc572db7f8
objektwerks <objektwerks@runbox.com>
2021-06-03 08:13:08 -0400

    edited cors handler

--- jvm/src/main/scala/pool/CorsHandler.scala
+++ jvm/src/main/scala/pool/CorsHandler.scala
@@ -1,8 +1,8 @@
 package pool
 
 import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.headers._
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Directive0, Route}
 
@@ -21,8 +21,11 @@ trait CorsHandler {
   def addAccessControlHeaders: Directive0 = respondWithHeaders( corsResponseHeaders )
 
   def preflightRequestHandler: Route = options {
-    complete( HttpResponse( StatusCodes.OK )
-      .withHeaders( `Access-Control-Allow-Methods`(GET, POST, PUT, DELETE, PATCH, OPTIONS) ) )
+    complete(
+      HttpResponse().withHeaders(
+        `Access-Control-Allow-Methods`(GET, POST, PUT, DELETE, PATCH, OPTIONS)
+      )
+    )
   }
 
   def addCORSHeaders(response: HttpResponse): HttpResponse = response.withHeaders( corsResponseHeaders )
