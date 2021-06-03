ec36d03b96e6c5483a846dcfcd5c6e71e3a0099d
objektwerks <objektwerks@runbox.com>
2021-06-03 08:03:28 -0400

    edited cors handler

--- jvm/src/main/scala/pool/CorsHandler.scala
+++ jvm/src/main/scala/pool/CorsHandler.scala
@@ -21,7 +21,8 @@ trait CorsHandler {
   def addAccessControlHeaders: Directive0 = respondWithHeaders( corsResponseHeaders )
 
   def preflightRequestHandler: Route = options {
-    complete( HttpResponse( StatusCodes.OK ).withHeaders( `Access-Control-Allow-Methods`(OPTIONS, GET, POST, PUT, DELETE) ) )
+    complete( HttpResponse( StatusCodes.OK )
+      .withHeaders( `Access-Control-Allow-Methods`(GET, POST, PUT, DELETE, PATCH, OPTIONS) ) )
   }
 
   def addCORSHeaders(response: HttpResponse): HttpResponse = response.withHeaders( corsResponseHeaders )
