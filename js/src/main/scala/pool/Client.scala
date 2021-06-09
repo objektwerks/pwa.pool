82b88a5554c7d3e5b7adf0ec2271f9a562fef1ad
objektwerks <objektwerks@runbox.com>
2021-06-09 09:36:32 -0400

    added Login component, extracting it from Client

--- js/src/main/scala/pool/Client.scala
+++ js/src/main/scala/pool/Client.scala
@@ -27,46 +27,7 @@ class Client(publicUrl: String, apiUrl: String) extends js.Object {
          "Register" ),
       a( href("#"),
          cls("w3-bar-item w3-button"),
-         onClick --> (_ => renderRoot( renderLogin.amend( display("block") ) ) ),
+         onClick --> (_ => renderRoot( Login.render(context, model).amend( display("block") ) ) ),
          "Login" )
-    )
-
-  def renderLogin: Div =
-    div( idAttr("login"), cls("w3-modal"),
-      div( cls("w3-modal-content w3-card w3-animate-left"),
-        div( cls("w3-row"),
-          div( cls("w3-col"), width("15%"),
-            label( cls("w3-left-align w3-text-indigo"), "Email:" )
-          ),
-          div( cls("w3-col"), width("85%"),
-            input( cls("w3-input w3-hover-light-gray w3-text-indigo"), typ("email"), required(true), autoFocus(true),
-              onChange.mapToValue.filter(_.nonEmpty) --> model.email
-            )
-          )
-        ),
-        div( cls("w3-row"),
-          div( cls("w3-col"), width("15%"),
-            label( cls("w3-left-align w3-text-indigo"), "Pin:" )
-          ),
-          div( cls("w3-col"), width("85%"),
-            input( cls("w3-input w3-hover-light-gray w3-text-indigo"), typ("number"), required(true),
-              onChange.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> model.pin
-            )
-          )
-        ),
-        div( cls("w3-row w3-padding-16"),
-          button( cls("w3-btn w3-text-indigo"),
-            onClick.mapTo { 
-              document.getElementById("login").setAttribute("style", "display: none")
-              SignIn(model.email.now(), model.pin.now()) 
-            } --> context.commandObserver,
-            "Login"
-          ),
-          button( cls("w3-btn w3-text-indigo"),
-            onClick --> (_ => document.getElementById("login").setAttribute("style", "display: none") ),
-            "Cancel"
-          )
-        )
-      )
     )
 }
