ba3c7d1c8cdfdbd1114d7eabb3cf0fe568bec6ee
objektwerks <objektwerks@runbox.com>
2021-06-09 09:45:45 -0400

    reformatted navigation for clarity

--- js/src/main/scala/pool/Navigation.scala
+++ js/src/main/scala/pool/Navigation.scala
@@ -5,7 +5,8 @@ import com.raquo.laminar.api.L._
 object Navigation {
   def render(context: Context, model: Model): Div =
     div( cls("w3-bar w3-white w3-text-indigo"),
-      a( href("#"), cls("w3-bar-item w3-button"),
+      a( href("#"),
+         cls("w3-bar-item w3-button"),
          onClick --> (_ => Register.render(context, model).amend( display("block") ) ),
          "Register" ),
       a( href("#"),
