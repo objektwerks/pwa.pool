19b32ae9ca71397deab56e27a4609b82aaa61c7b
objektwerks <objektwerks@runbox.com>
2021-06-11 02:58:17 -0400

    edited entities

--- js/src/main/scala/pool/Entities.scala
+++ js/src/main/scala/pool/Entities.scala
@@ -8,10 +8,10 @@ object Entities {
     println(model)
     div( cls("w3-container"),
       div( cls("w3-panel w3-indigo"),
-        h3("Pools"),
-        ul( idAttr("pools"), cls("w3-ul w3-hoverable") ),
-        button( cls("w3-btn w3-text-indigo"), "Add" )
-      )
+        h3("Pools")
+      ),
+      ul( idAttr("pools"), cls("w3-ul w3-hoverable") ),
+      button( cls("w3-btn w3-text-indigo"), "Add" )
     ) 
   }
 }
