4b0720d377125f687ffd73563e14dc7df53a29c3
objektwerks <objektwerks@runbox.com>
2021-06-11 02:54:47 -0400

    edited entities

--- js/src/main/scala/pool/Entities.scala
+++ js/src/main/scala/pool/Entities.scala
@@ -6,10 +6,10 @@ object Entities {
   def render(context: Context, model: Model): Div = {
     println(context)
     println(model)
-    div( idAttr("entities"), cls("w3-container"),
+    div( cls("w3-container"),
       div( cls("w3-panel w3-indigo"),
         h3("Pools"),
-        ul( idAttr("pools"), cls("w3-ul") ),
+        ul( idAttr("pools"), cls("w3-ul w3-hoverable") ),
         button( cls("w3-btn w3-text-indigo"), "Add" )
       )
     ) 
