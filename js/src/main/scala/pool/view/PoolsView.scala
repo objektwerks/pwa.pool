5ba18d53c3022822fa39c6fed9d6feb969f077ce
objektwerks <objektwerks@runbox.com>
2021-06-12 08:51:20 -0400

    edited pools view

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -8,10 +8,9 @@ object PoolsView {
   def render(context: Context): Div = {
     println(context)
     div( idAttr("poolsView"), cls("w3-container"),
-      div( cls("w3-panel w3-indigo"),
-        h3("Pools")
+      ul( idAttr("pools"), cls("w3-ul w3-hoverable"),
+        li( h3("Pools") )
       ),
-      ul( idAttr("pools"), cls("w3-ul w3-hoverable") ),
       button( cls("w3-btn w3-text-indigo"), "Add" ),
       button( cls("w3-btn w3-text-indigo"), "Update" )
     )
