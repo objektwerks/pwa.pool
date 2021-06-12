b95ee1e09abbe7d7117b4b0d76e14f1d4e8b5e81
objektwerks <objektwerks@runbox.com>
2021-06-12 08:58:15 -0400

    edited pools view

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -11,8 +11,10 @@ object PoolsView {
       ul( idAttr("pools"), cls("w3-ul w3-hoverable"),
         li( h3("Pools") )
       ),
-      button( cls("w3-btn w3-text-indigo"), "Add" ),
-      button( cls("w3-btn w3-text-indigo"), "Update" )
+      div( cls("w3-row w3-padding-16"),
+        button( cls("w3-btn w3-text-indigo"), "Add" ),
+        button( cls("w3-btn w3-text-indigo"), "Update" )
+      )
     )
   }
 }
