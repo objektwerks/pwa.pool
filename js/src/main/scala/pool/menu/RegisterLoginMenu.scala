2627cccc51be4a8ed5b868843eb54fcfd9cccbed
objektwerks <objektwerks@runbox.com>
2021-06-14 13:24:50 -0400

    add deactivate and reactivate links to register login menu

--- js/src/main/scala/pool/menu/RegisterLoginMenu.scala
+++ js/src/main/scala/pool/menu/RegisterLoginMenu.scala
@@ -13,6 +13,14 @@ object RegisterLoginMenu {
          cls("w3-bar-item w3-button"),
          onClick --> (_ => loginDialog.amend( display("block") ) ),
          "Login" ),
+      a( href("#"),
+        cls("w3-bar-item w3-button"),
+        onClick --> (_ => println("Deactivate licensee dialog todo!" ) ),
+        "Deactivate" ),
+      a( href("#"),
+        cls("w3-bar-item w3-button"),
+        onClick --> (_ => println("Reactivate licensee dialog todo!" ) ),
+        "Reactivate" ),
       registerDialog,
       loginDialog
     )
