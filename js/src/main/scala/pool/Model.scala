d0aa62e27a41a4adee91691234f80e9828f7c5a2
objektwerks <objektwerks@runbox.com>
2021-06-15 07:37:13 -0400

    added license to model

--- js/src/main/scala/pool/Model.scala
+++ js/src/main/scala/pool/Model.scala
@@ -4,4 +4,5 @@ import com.raquo.laminar.api.L._
 
 case class Model(email: Var[String] = Var(""),
                  pin: Var[Int] = Var(0),
+                 license: Var[String] = Var(""),
                  licensee: Var[Option[Licensee]] = Var(None)) extends Product with Serializable
