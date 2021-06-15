425e7e26300b7a77991c37adaf0ff39f58ea497a
objektwerks <objektwerks@runbox.com>
2021-06-15 07:27:54 -0400

    added deactivate and reactivate dialog href to menu

--- js/src/main/scala/pool/menu/CommandMenu.scala
+++ js/src/main/scala/pool/menu/CommandMenu.scala
@@ -3,8 +3,13 @@ package pool.menu
 import com.raquo.laminar.api.L._
 
 object CommandMenu {
-  def apply(registerDialog: Div, loginDialog: Div): Div =
-    div( idAttr("registerLoginMenu"), cls("w3-bar w3-white w3-text-indigo"),
+  val id = getClass.getSimpleName
+
+  def apply(registerDialog: Div,
+            loginDialog: Div,
+            deactivateDialog: Div,
+            reactivateDialog: Div): Div =
+    div( idAttr(id), cls("w3-bar w3-white w3-text-indigo"),
       a( href("#"),
          cls("w3-bar-item w3-button"),
          onClick --> (_ => registerDialog.amend( display("block") ) ),
@@ -15,13 +20,15 @@ object CommandMenu {
          "Login" ),
       a( href("#"),
         cls("w3-bar-item w3-button"),
-        onClick --> (_ => println("Deactivate licensee dialog todo!" ) ),
+        onClick --> (_ => deactivateDialog.amend( display("block") ) ),
         "Deactivate" ),
       a( href("#"),
         cls("w3-bar-item w3-button"),
-        onClick --> (_ => println("Reactivate licensee dialog todo!" ) ),
+        onClick --> (_ => reactivateDialog.amend( display("block") ) ),
         "Reactivate" ),
       registerDialog,
-      loginDialog
+      loginDialog,
+      deactivateDialog,
+      reactivateDialog
     )
 }
