7cd1fa9ea8e7e32908a93ffbeb51f680db9d277d
objektwerks <objektwerks@runbox.com>
2021-06-16 09:05:52 -0400

    simplified urls

--- js/src/main/scala/pool/Urls.scala
+++ js/src/main/scala/pool/Urls.scala
@@ -1,21 +1,11 @@
 package pool
 
-object Urls {
-    def apply(publicUrl: String, apiUrl: String): Urls = Urls(
-      signup = s"$publicUrl/signup",
-      signin = s"$publicUrl/signin",
-      deactivate = s"$publicUrl/deactivatelicensee",
-      reactivate = s"$publicUrl/reactivatelicensee",
-      pools = s"$apiUrl/pools",
-      poolsAdd = s"$apiUrl/pools/add",
-      poolsUpdate = s"$apiUrl/pools/update"
-    )
+case class Urls(publicUrl: String, apiUrl: String) extends Product with Serializable {
+  val signup = s"$publicUrl/signup"
+  val signin = s"$publicUrl/signin"
+  val deactivate = s"$publicUrl/deactivatelicensee"
+  val reactivate = s"$publicUrl/reactivatelicensee"
+  val pools = s"$apiUrl/pools"
+  val poolsAdd = s"$apiUrl/pools/add"
+  val poolsUpdate = s"$apiUrl/pools/update"
 }
-
-case class Urls(signup: String,
-                signin: String, 
-                deactivate: String, 
-                reactivate: String, 
-                pools: String,
-                poolsAdd: String,
-                poolsUpdate: String) extends Product with Serializable
