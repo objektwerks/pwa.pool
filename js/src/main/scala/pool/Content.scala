62b01cfee184511a123ee7656fd19a29b7aa1766
objektwerks <objektwerks@runbox.com>
2021-06-16 08:47:49 -0400

    renamed val in content

--- js/src/main/scala/pool/Content.scala
+++ js/src/main/scala/pool/Content.scala
@@ -3,9 +3,9 @@ package pool
 import com.raquo.laminar.api.L._
 
 object Content {
-  def apply(registerLoginMenu: Div, poolsView: Div): Div =
+  def apply(commandMenu: Div, poolsView: Div): Div =
     div( idAttr("content"), cls("w3-container"),
-      registerLoginMenu,
+      commandMenu,
       poolsView
     )
 }
