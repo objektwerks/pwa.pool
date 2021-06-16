6f3f3fe03460d4900dc091188aba19d95aa6a2f1
objektwerks <objektwerks@runbox.com>
2021-06-16 07:31:59 -0400

    added status panel and event bus to register dialog

--- js/src/main/scala/pool/dialog/RegisterDialog.scala
+++ js/src/main/scala/pool/dialog/RegisterDialog.scala
@@ -8,11 +8,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object RegisterDialog {
   val id = getClass.getSimpleName
+  val statusEventBus = new EventBus[String]
 
   def apply(context: Context): Div =
     div( idAttr(id), cls("w3-modal"),
       div( cls("w3-container"),
         div( cls("w3-modal-content"),
+          div( cls("w3-panel w3-indigo"),
+            label( cls("w3-left-align"), "Status:" ),
+            child.text <-- statusEventBus.events
+          ),
           div( cls("w3-row"),
             div( cls("w3-col"), width("15%"),
               label( cls("w3-left-align w3-text-indigo"), "Email:" )
@@ -30,12 +35,12 @@ object RegisterDialog {
                 ServerProxy.post(context.urls.signup, Licensee.emptyLicense, command).foreach {
                   case Right(event) => event match {
                     case signedup: SignedUp =>
-                      println(s"signedup: $signedup")
+                      statusEventBus.emit( s"Success: $signedup" )
                       context.model.licensee.set( Some(signedup.licensee) )
                       context.displayToNone(id)
-                    case _ => println(s"invalid event: $event")
+                    case _ => statusEventBus.emit( s"Invalid: $event" )
                   }
-                  case Left(fault) => println(s"fault: $fault")
+                  case Left(fault) => statusEventBus.emit( s"Failure: $fault" )
                 }
                },
               "Register"
