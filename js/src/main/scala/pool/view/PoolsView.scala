03161660de14565cbe5e9e099e452b5f2032a4d6
objektwerks <objektwerks@runbox.com>
2021-06-18 07:15:42 -0400

    refactored pools view header and list

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -8,9 +8,10 @@ object PoolsView {
   def apply(context: Context): Div = {
     println(context)
     div( cls("w3-container"),
-      ul( idAttr("pools"), cls("w3-ul w3-hoverable"),
-        li( h3("Pools") )
+      div( cls("w3-panel w3-indigo"),
+        label("Pools")
       ),
+      ul( idAttr("pools"), cls("w3-ul w3-hoverable") ),
       div( cls("w3-row w3-padding-16"),
         button( cls("w3-btn w3-text-indigo"), "Add" ),
         button( cls("w3-btn w3-text-indigo"), "Update" )
