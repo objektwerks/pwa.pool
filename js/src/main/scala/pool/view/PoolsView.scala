794cf86078f31efa93ad5b93f7e084f88fbeb81c
objektwerks <objektwerks@runbox.com>
2021-06-20 09:09:19 -0400

    added id to pools view

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -5,16 +5,18 @@ import com.raquo.laminar.api.L._
 import pool.Context
 
 object PoolsView {
+  val id = getClass.getSimpleName
+
   def apply(context: Context): Div = {
     println(context)
-    div( cls("w3-container"), display("none"),
-      div( cls("w3-panel w3-indigo"),
+    div(idAttr(id), cls("w3-container"), display("none"),
+      div(cls("w3-panel w3-indigo"),
         label("Pools")
       ),
-      ul( idAttr("pools"), cls("w3-ul w3-hoverable") ),
-      div( cls("w3-row w3-padding-16"),
-        button( cls("w3-btn w3-text-indigo"), "Add" ),
-        button( cls("w3-btn w3-text-indigo"), "Update" )
+      ul(idAttr("pools"), cls("w3-ul w3-hoverable")),
+      div(cls("w3-row w3-padding-16"),
+        button(cls("w3-btn w3-text-indigo"), "Add"),
+        button(cls("w3-btn w3-text-indigo"), "Update")
       )
     )
   }
