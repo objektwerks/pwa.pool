1f50eb228cfbbff7eb739168dee5977d7fd2f570
objektwerks <objektwerks@runbox.com>
2021-06-20 09:06:09 -0400

    set pools view display to none

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -7,7 +7,7 @@ import pool.Context
 object PoolsView {
   def apply(context: Context): Div = {
     println(context)
-    div( cls("w3-container"),
+    div( cls("w3-container"), display("none"),
       div( cls("w3-panel w3-indigo"),
         label("Pools")
       ),
