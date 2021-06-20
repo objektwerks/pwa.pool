10eaca113624d189476b089088335a2be9de7dbe
objektwerks <objektwerks@runbox.com>
2021-06-20 13:46:58 -0400

    add an id to each home menu item for each command dialog

--- js/src/main/scala/pool/menu/HomeMenu.scala
+++ js/src/main/scala/pool/menu/HomeMenu.scala
@@ -1,29 +1,30 @@
 package pool.menu
 
 import com.raquo.laminar.api.L._
+import pool.dialog.{DeactivateDialog, LoginDialog, ReactivateDialog, RegisterDialog}
 
 object HomeMenu {
   val id = getClass.getSimpleName
+  val registerId = id + RegisterDialog.id
+  val loginId = id + LoginDialog.id
+  val deactivateId = id + DeactivateDialog.id
+  val reactivateId = id + ReactivateDialog.id
 
   def apply(registerDialog: Div,
             loginDialog: Div,
             deactivateDialog: Div,
             reactivateDialog: Div): Div =
     div(idAttr(id), cls("w3-bar w3-white w3-text-indigo"),
-      a(href("#"),
-        cls("w3-bar-item w3-button"),
+      a(idAttr(registerId), href("#"), cls("w3-bar-item w3-button"),
         onClick --> (_ => registerDialog.amend(display("block"))),
         "Register"),
-      a(href("#"),
-        cls("w3-bar-item w3-button"),
+      a(idAttr(loginId), href("#"), cls("w3-bar-item w3-button"),
         onClick --> (_ => loginDialog.amend(display("block"))),
         "Login"),
-      a(href("#"),
-        cls("w3-bar-item w3-button"),
+      a(idAttr(deactivateId), href("#"), cls("w3-bar-item w3-button"),
         onClick --> (_ => deactivateDialog.amend(display("block"))),
         "Deactivate"),
-      a(href("#"),
-        cls("w3-bar-item w3-button"),
+      a(idAttr(reactivateId), href("#"), cls("w3-bar-item w3-button"),
         onClick --> (_ => reactivateDialog.amend(display("block"))),
         "Reactivate"),
       registerDialog,
