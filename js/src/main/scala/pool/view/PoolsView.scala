75ad36d32d6937b4fb8041078f762f4f9a05bc24
objektwerks <objektwerks@runbox.com>
2021-06-24 10:52:03 -0400

    added header to pools view

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -10,8 +10,8 @@ object PoolsView {
   def apply(context: Context): Div = {
     println(context)
     div(idAttr(id), cls("w3-container"), display("none"),
-      div(cls("w3-panel w3-indigo"),
-        label("Pools")
+      header(cls("w3-container w3-indigo"),
+        h3("Pools")
       ),
       ul(idAttr("pools"), cls("w3-ul w3-hoverable")),
       div(cls("w3-row w3-padding-16"),
