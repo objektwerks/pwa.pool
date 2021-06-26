05bfe537ebf18f39471c2485f6d45bc410d6e9bc
objektwerks <objektwerks@runbox.com>
2021-06-26 08:42:17 -0400

    added name to pool entity

--- shared/src/main/scala/pool/Entity.scala
+++ shared/src/main/scala/pool/Entity.scala
@@ -37,6 +37,7 @@ final case class License(key: String) extends Entity
 
 final case class Pool(id: Int = 0,
                       license: String,
+                      name: String,
                       built: Int,
                       lat: Double,
                       lon: Double,
