d714b966590012efc3dc14e1893b1dc79af12950
objektwerks <objektwerks@runbox.com>
2021-06-27 08:24:40 -0400

    enhanced serializer test

--- shared/src/test/scala/pool/SerializerTest.scala
+++ shared/src/test/scala/pool/SerializerTest.scala
@@ -12,14 +12,16 @@ class SerializerTest extends AnyFunSuite with Matchers {
     val licenseeJson = write(licensee)
     println(licensee)
     println(licenseeJson)
+    println(read[Licensee](licenseeJson))
     licensee shouldBe read[Licensee](licenseeJson)
   }
 
   test("pool") {
-    val pool = Pool(id = 0, license = "abc123", name = "pool", built = 1991, lat = 26.85, lon = 82.29, volume = 10000)
+    val pool = Pool(license = "abc123", name = "pool", built = 1991, lat = 26.85, lon = 82.29, volume = 10000)
     val poolJson = write(pool)
     println(pool)
     println(poolJson)
+    println(read[Pool](poolJson))
     pool shouldBe read[Pool](poolJson)
   }
 }
