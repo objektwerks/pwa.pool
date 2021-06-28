6f0c7ff3c97839e646c07b68e438cc4409f68f6f
objektwerks <objektwerks@runbox.com>
2021-06-28 07:34:42 -0400

    in state, replaced seq with array

--- shared/src/main/scala/pool/State.scala
+++ shared/src/main/scala/pool/State.scala
@@ -6,26 +6,26 @@ final case class Id(id: Int) extends State
 
 final case class Count(count: Int) extends State
 
-final case class Pools(pools: Seq[Pool]) extends State
+final case class Pools(pools: Array[Pool]) extends State
 
-final case class Surfaces(surfaces: Seq[Surface]) extends State
+final case class Surfaces(surfaces: Array[Surface]) extends State
 
-final case class Pumps(pumps: Seq[Pump]) extends State
+final case class Pumps(pumps: Array[Pump]) extends State
 
-final case class Timers(timers: Seq[Timer]) extends State
+final case class Timers(timers: Array[Timer]) extends State
 
-final case class TimerSettings(timerSettings: Seq[TimerSetting]) extends State
+final case class TimerSettings(timerSettings: Array[TimerSetting]) extends State
 
-final case class Heaters(heaters: Seq[Heater]) extends State
+final case class Heaters(heaters: Array[Heater]) extends State
 
-final case class HeaterSettings(heaterSettings: Seq[HeaterSetting]) extends State
+final case class HeaterSettings(heaterSettings: Array[HeaterSetting]) extends State
 
-final case class Measurements(measurements: Seq[Measurement]) extends State
+final case class Measurements(measurements: Array[Measurement]) extends State
 
-final case class Cleanings(cleanings: Seq[Cleaning]) extends State
+final case class Cleanings(cleanings: Array[Cleaning]) extends State
 
-final case class Chemicals(chemicals: Seq[Chemical]) extends State
+final case class Chemicals(chemicals: Array[Chemical]) extends State
 
-final case class Supplies(supplies: Seq[Supply]) extends State
+final case class Supplies(supplies: Array[Supply]) extends State
 
-final case class Repairs(repairs: Seq[Repair]) extends State
+final case class Repairs(repairs: Array[Repair]) extends State
