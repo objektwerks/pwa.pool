970793868b56259a352ee4c389ef8a5fbf0d848b
objektwerks <objektwerks@runbox.com>
2021-06-29 12:57:10 -0400

    refactored pools view command bar

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -9,12 +9,12 @@ object PoolsView {
 
   def apply(context: Context): Div = {
     println(context)
-    div(idAttr(id), cls("w3-container w3-margin"), display("none"),
-      p(cls("w3-indigo"), "Pools"),
+    div(idAttr(id), cls("w3-container"), display("none"),
+      h6(cls("w3-indigo"), "Pools"),
       ul(idAttr("pools"), cls("w3-ul w3-hoverable")),
-      div(cls("w3-row w3-padding-16"),
-        button(cls("w3-btn w3-text-indigo"), "Add"),
-        button(cls("w3-btn w3-text-indigo"), "Update")
+      div(cls("w3-bar"),
+        button(cls("w3-bar-item w3-button w3-margin w3-text-indigo"), "Add"),
+        button(cls("w3-bar-item w3-button w3-margin w3-text-indigo"), "Update")
       )
     )
   }
