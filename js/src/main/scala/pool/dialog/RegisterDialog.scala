bc0b0b563a1e895546652b03fd0c132e3cfaab4b
objektwerks <objektwerks@runbox.com>
2021-06-29 07:51:41 -0400

    edited header of register dialog

--- js/src/main/scala/pool/dialog/RegisterDialog.scala
+++ js/src/main/scala/pool/dialog/RegisterDialog.scala
@@ -16,9 +16,7 @@ object RegisterDialog {
     div(idAttr(id), cls("w3-modal"),
       div(cls("w3-container"),
         div(cls("w3-modal-content"),
-          header(cls("w3-container w3-indigo"),
-            h3("Register")
-          ),
+          p(cls("w3-indigo"), "Register"),
           div(cls("w3-panel w3-red"),
             child.text <-- errors.events
           ),
