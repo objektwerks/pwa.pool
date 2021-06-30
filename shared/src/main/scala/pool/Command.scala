6d6ef68ddad9a06f9a82df4f165389daea0f696c
objektwerks <objektwerks@runbox.com>
2021-06-30 07:48:16 -0400

    simplified deactivate and reactivate licensee commands

--- shared/src/main/scala/pool/Command.scala
+++ shared/src/main/scala/pool/Command.scala
@@ -4,8 +4,8 @@ sealed trait Command extends Product with Serializable
 
 final case class SignUp(email: String) extends Command
 
-final case class SignIn(email: String, pin: Int) extends Command
+final case class SignIn(pin: Int) extends Command
 
-final case class DeactivateLicensee(license: String, email: String, pin: Int) extends Command
+final case class DeactivateLicensee(license: String) extends Command
 
-final case class ReactivateLicensee(license: String, email: String, pin: Int) extends Command
+final case class ReactivateLicensee(license: String) extends Command
