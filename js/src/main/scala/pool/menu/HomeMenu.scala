0f9a8958faef8dd2b9a68186be76f7fe7d9f3e34
objektwerks <objektwerks@runbox.com>
2021-07-02 06:26:48 -0400

    refactored home menu ids for clarity

--- js/src/main/scala/pool/menu/HomeMenu.scala
+++ js/src/main/scala/pool/menu/HomeMenu.scala
@@ -3,35 +3,38 @@ package pool.menu
 import com.raquo.laminar.api.L._
 
 import pool.Context
-import pool.dialog.AccountDialog.{deactivateId, reactivateId}
 import pool.dialog.{AccountDialog, LoginDialog, RegisterDialog}
 
 object HomeMenu {
   val id = getClass.getSimpleName
-  val registerId = id + "-" + RegisterDialog.id
-  val loginId = id + "-" + LoginDialog.id
-  val accountId = id + "-" + AccountDialog.id
+  val registerMenuItemId = id + "-" + RegisterDialog.id
+  val loginMenuItemId = id + "-" + LoginDialog.id
+  val accountMenuItemId = id + "-" + AccountDialog.id
 
   def apply(context: Context,
             registerDialog: Div,
             loginDialog: Div,
             accountDialog: Div): Div =
     div(idAttr(id), cls("w3-bar w3-margin w3-white w3-text-indigo"),
-      a(idAttr(registerId), href("#"), cls("w3-bar-item w3-button"),
-        onClick --> (_ => registerDialog.amend(display("block"))),
+      a(idAttr(registerMenuItemId), href("#"), cls("w3-bar-item w3-button"),
+        onClick --> { _ =>
+          registerDialog.amend(display("block"))
+        },
         "Register"),
-      a(idAttr(loginId), href("#"), cls("w3-bar-item w3-button"),
-        onClick --> (_ => loginDialog.amend(display("block"))),
+      a(idAttr(loginMenuItemId), href("#"), cls("w3-bar-item w3-button"),
+        onClick --> { _ =>
+          loginDialog.amend(display("block"))
+        },
         "Login"),
-      a(idAttr(accountId), href("#"), cls("w3-bar-item w3-button"), display("none"),
+      a(idAttr(accountMenuItemId), href("#"), cls("w3-bar-item w3-button"), display("none"),
         onClick --> { _ =>
           accountDialog.amend(display("block"))
           if (context.licensee.now().deactivated == 0) {
-            context.show(deactivateId)
-            context.hide(reactivateId)
+            context.show(AccountDialog.deactivateButtonId)
+            context.hide(AccountDialog.reactivateButtonId)
           } else {
-            context.hide(deactivateId)
-            context.show(reactivateId)
+            context.hide(AccountDialog.deactivateButtonId)
+            context.show(AccountDialog.reactivateButtonId)
           }
         },
         "Account"),
