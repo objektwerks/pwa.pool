598637a95568994635e572d29e8bf3c1133db155
objektwerks <objektwerks@runbox.com>
2021-07-10 06:44:51 -0400

    added field component

---
+++ js/src/main/scala/pool/component/Field.scala
@@ -0,0 +1,12 @@
+package pool.component
+
+import com.raquo.laminar.api.L._
+
+object Field {
+  def apply(label: Label,
+            input: Input): Div =
+    div(cls("w3-row w3-margin"),
+      label,
+      input
+    )
+}
