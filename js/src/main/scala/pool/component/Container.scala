1c69490d27afc890b088faed0775918527d45cee
objektwerks <objektwerks@runbox.com>
2021-07-11 07:22:48 -0400

    moved container

--- js/src/main/scala/pool/component/Container.scala
+++ js/src/main/scala/pool/component/Container.scala
@@ -1,13 +1,11 @@
-package pool
+package pool.component
 
 import com.raquo.laminar.api.L._
 
 object Container {
-  val id = getClass.getSimpleName
-
   def apply(homeMenu: Div,
             poolsView: Div): Div =
-    div(idAttr(id), cls("w3-container"),
+    div(cls("w3-container"),
       homeMenu,
       poolsView
     )
