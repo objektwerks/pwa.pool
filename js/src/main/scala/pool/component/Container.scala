dd35255d1076c8911f39af7dec23249651651a99
objektwerks <objektwerks@runbox.com>
2021-07-15 11:25:36 -0400

    simplified container with * divs

--- js/src/main/scala/pool/component/Container.scala
+++ js/src/main/scala/pool/component/Container.scala
@@ -3,7 +3,5 @@ package pool.component
 import com.raquo.laminar.api.L._
 
 object Container {
-  def apply(divs: Div*): Div = {
-    div(cls("w3-container")).amendThis( _ => divs )
-  }
+  def apply(divs: Div*): Div = div(cls("w3-container"), divs )
 }
