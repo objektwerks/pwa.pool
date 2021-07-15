a5a83aeb9ea3bd3e90fcb98371a960184300ccbd
objektwerks <objektwerks@runbox.com>
2021-07-15 07:50:14 -0400

    added display to menu item

--- js/src/main/scala/pool/component/MenuItem.scala
+++ js/src/main/scala/pool/component/MenuItem.scala
@@ -3,6 +3,8 @@ package pool.component
 import com.raquo.laminar.api.L._
 
 object MenuItem {
-  def apply(id: String, name: String): Anchor =
-    a(idAttr(id), href("#"), cls("w3-bar-item w3-button"), name)
+  def apply(id: String,
+            name: String,
+            isDisplayed: String = "block"): Anchor =
+    a(idAttr(id), href("#"), cls("w3-bar-item w3-button"), display(isDisplayed), name)
 }
