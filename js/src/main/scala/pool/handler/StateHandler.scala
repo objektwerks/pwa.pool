00790f1098b7ab41ffafe88cb11e6604d16fb591
objektwerks <objektwerks@runbox.com>
2021-07-16 08:37:26 -0400

    replaced console.info with println

--- js/src/main/scala/pool/handler/StateHandler.scala
+++ js/src/main/scala/pool/handler/StateHandler.scala
@@ -2,8 +2,6 @@ package pool.handler
 
 import com.raquo.laminar.api.L._
 
-import org.scalajs.dom.console
-
 import pool.{Context, Fault, State}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -18,14 +16,14 @@ object StateHandler {
     response.onComplete {
       case Success(either) => either match {
         case Right(state) =>
-          console.info(s"State: $state")
+          println(s"State: $state")
           handler(context, errors, state)
         case Left(fault) =>
-          console.error(s"Fault: $fault")
+          println(s"Fault: $fault")
           errors.emit(s"Fault: $fault")
       }
       case Failure(failure) =>
-        console.error(s"Failure: $failure")
+        println(s"Failure: $failure")
         errors.emit(s"Failure: $failure")
     }
   }
