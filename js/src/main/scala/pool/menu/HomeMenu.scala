25d0ff408ef21fe0e15b722f9176bdb9218ab8f9
objektwerks <objektwerks@runbox.com>
2021-07-16 08:03:06 -0400

    added menu item bar to home menu

--- js/src/main/scala/pool/menu/HomeMenu.scala
+++ js/src/main/scala/pool/menu/HomeMenu.scala
@@ -3,7 +3,7 @@ package pool.menu
 import com.raquo.laminar.api.L._
 
 import pool.Context
-import pool.component.MenuItem
+import pool.component._
 import pool.dialog.AccountDialog
 import pool.view.PoolsView
 
@@ -18,38 +18,40 @@ object HomeMenu {
             registerDialog: Div,
             loginDialog: Div,
             accountDialog: Div): Div =
-    div(idAttr(id), cls("w3-bar w3-margin w3-white w3-text-indigo"),
-      MenuItem(id = registerMenuItemId, name = "Register").amend {
-        onClick --> { _ =>
-          registerDialog.amend(display("block"))
-        }
-      },
-      MenuItem(id = loginMenuItemId, name = "Login").amend {
-        onClick --> { _ =>
-          loginDialog.amend(display("block"))
-        }
-      },
-      MenuItem(id = accountMenuItemId, name = "Account", isDisplayed = "none").amend {
-        display("none")
-        onClick --> { _ =>
-          accountDialog.amend(display("block"))
-          if (context.account.now().deactivated == 0) {
-            context.show(AccountDialog.deactivateButtonId)
-            context.hide(AccountDialog.reactivateButtonId)
-          } else {
-            context.hide(AccountDialog.deactivateButtonId)
-            context.show(AccountDialog.reactivateButtonId)
+    Container(
+      MenuItemBar(
+        MenuItem(id = registerMenuItemId, name = "Register").amend {
+          onClick --> { _ =>
+            registerDialog.amend(display("block"))
+          }
+        },
+        MenuItem(id = loginMenuItemId, name = "Login").amend {
+          onClick --> { _ =>
+            loginDialog.amend(display("block"))
+          }
+        },
+        MenuItem(id = accountMenuItemId, name = "Account", isDisplayed = "none").amend {
+          display("none")
+          onClick --> { _ =>
+            accountDialog.amend(display("block"))
+            if (context.account.now().deactivated == 0) {
+              context.show(AccountDialog.deactivateButtonId)
+              context.hide(AccountDialog.reactivateButtonId)
+            } else {
+              context.hide(AccountDialog.deactivateButtonId)
+              context.show(AccountDialog.reactivateButtonId)
+            }
+          }
+        },
+        MenuItem(id = poolsMenuItemId, name = "Pools", isDisplayed = "none").amend {
+          display("none")
+          onClick --> { _ =>
+            PoolsView.pools(context)
+            context.show(PoolsView.id)
+            context.hide(poolsMenuItemId)
           }
         }
-      },
-      MenuItem(id = poolsMenuItemId, name = "Pools", isDisplayed = "none").amend {
-        display("none")
-        onClick --> { _ =>
-          PoolsView.pools(context)
-          context.show(PoolsView.id)
-          context.hide(poolsMenuItemId)
-        }
-      },
+      ),
       registerDialog,
       loginDialog,
       accountDialog
