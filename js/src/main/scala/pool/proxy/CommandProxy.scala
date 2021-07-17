99b7749d47d91f87604608101e493d31f3f6922e
objektwerks <objektwerks@runbox.com>
2021-07-17 07:30:37 -0400

    edited command proxy logging

--- js/src/main/scala/pool/proxy/CommandProxy.scala
+++ js/src/main/scala/pool/proxy/CommandProxy.scala
@@ -2,7 +2,7 @@ package pool.proxy
 
 import org.scalajs.dom.ext.Ajax
 
-import pool.{Command, Event, Fault, Serializers}
+import pool.{Command, Context, Event, Fault, Serializers}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -13,7 +13,7 @@ object CommandProxy extends Proxy {
   import upickle.default._
 
   def post(url: String, license: String, command: Command): Future[Either[Fault, Event]] = {
-    println(s"Command: $command")
+    Context.log(s"Command: $command")
     Ajax.post(url = url, headers = headers(license), data = write[Command](command)).map { xhr =>
       xhr.status match {
         case 200 => Try(read[Event](xhr.responseText)).fold(error => Left(log(error)), event => Right(event))
