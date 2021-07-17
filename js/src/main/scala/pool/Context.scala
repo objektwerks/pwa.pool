f21cbbab8afaa56d38c378f08f5df8a3da5dd1f7
objektwerks <objektwerks@runbox.com>
2021-07-17 07:28:55 -0400

    added common log method to context

--- js/src/main/scala/pool/Context.scala
+++ js/src/main/scala/pool/Context.scala
@@ -3,6 +3,9 @@ package pool
 import com.raquo.laminar.api.L._
 
 import org.scalajs.dom.document
+object Context {
+  def log(any: Any): Unit = println(any)
+}
 
 case class Context(publicUrl: String, apiUrl: String) extends Product with Serializable {
   val email = Var("")
@@ -18,6 +21,8 @@ case class Context(publicUrl: String, apiUrl: String) extends Product with Seria
   val poolsUrl = s"$apiUrl/pools"
   val poolsAddUrl = s"$apiUrl/pools/add"
   val poolsUpdateUrl = s"$apiUrl/pools/update"
+
+  def log(any: Any): Unit = Context.log(any)
 
   def show(id: String): Unit = document.getElementById(id).setAttribute("style", "display: block")
   def hide(id: String): Unit = document.getElementById(id).setAttribute("style", "display: none")
