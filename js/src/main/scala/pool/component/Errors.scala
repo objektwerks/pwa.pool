d13edfcf6176d10b56f46fa9614791079dce7733
objektwerks <objektwerks@runbox.com>
2021-07-21 07:00:41 -0400

    refactored errors component

--- js/src/main/scala/pool/component/Errors.scala
+++ js/src/main/scala/pool/component/Errors.scala
@@ -5,7 +5,7 @@ import com.raquo.laminar.api.L._
 object Errors {
   def apply(errors: EventBus[String]): Div =
     div(
-      cls("w3-panel w3-red"),
+      cls("w3-border-white w3-red"),
       child.text <-- errors.events
     )
 }
