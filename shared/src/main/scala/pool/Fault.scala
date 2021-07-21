fdaa3163f1eb31bf4243d123ac76749adb872fe8
objektwerks <objektwerks@runbox.com>
2021-07-21 11:37:32 -0400

    refactored fault

--- shared/src/main/scala/pool/Fault.scala
+++ shared/src/main/scala/pool/Fault.scala
@@ -1,7 +1,17 @@
 package pool
 
-final case class Fault(dateOf: Int = DateTime.currentDate,
-                       timeOf: Int = DateTime.currentTime,
-                       nanoOf: Int = DateTime.nano,
-                       code: Int = 500,
+final case class Fault(dateOf: Int,
+                       timeOf: Int,
+                       nanoOf: Int,
+                       code: Int,
                        cause: String) extends Product with Serializable
+
+object Fault {
+  def apply(code: Int = 500, cause: String): Fault = Fault(
+    dateOf = DateTime.currentDate,
+    timeOf = DateTime.currentTime,
+    nanoOf = DateTime.nano,
+    code = code,
+    cause = cause
+  )
+}
