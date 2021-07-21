c0bddce5a7534e6ff48bc00538a105ee68b0585d
objektwerks <objektwerks@runbox.com>
2021-07-21 07:01:01 -0400

    refactored messages component

--- js/src/main/scala/pool/component/Messages.scala
+++ js/src/main/scala/pool/component/Messages.scala
@@ -5,7 +5,7 @@ import com.raquo.laminar.api.L._
 object Messages {
   def apply(messages: EventBus[String]): Div =
     div(
-      cls("w3-panel w3-indigo"),
+      cls("w3-border-white w3-indigo"),
       child.text <-- messages.events
     )
 }
