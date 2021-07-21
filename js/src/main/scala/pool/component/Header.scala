994359139264f844adf41ed662189e9711b6dae0
objektwerks <objektwerks@runbox.com>
2021-07-21 11:37:55 -0400

    refactored header

--- js/src/main/scala/pool/component/Header.scala
+++ js/src/main/scala/pool/component/Header.scala
@@ -5,7 +5,7 @@ import com.raquo.laminar.api.L._
 object Header {
   def apply(name: String): Div =
     div(
-      cls("w3-panel w3-indigo"),
-      p(name)
+      cls("w3-indigo"),
+      h6(cls("w3-margin"), name)
     )
 }
