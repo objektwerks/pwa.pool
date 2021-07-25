04806bb97c81f1667782c97d53e1381283ef140f
objektwerks <objektwerks@runbox.com>
2021-07-25 08:16:20 -0400

    removed Unauthorized(401) code from all proxies; just look for 200, 400 and 500

--- js/src/main/scala/pool/proxy/EntityProxy.scala
+++ js/src/main/scala/pool/proxy/EntityProxy.scala
@@ -17,7 +17,7 @@ object EntityProxy extends Proxy {
     Ajax.post(url = url, headers = headers(license), data = write[Entity](entity)).map { xhr =>
       xhr.status match {
         case 200 => Try(read[State](xhr.responseText)).fold(error => Left(log(error)), state => Right(state))
-        case 400 | 401 | 500 => toFault(xhr.responseText)
+        case 400 | 500 => toFault(xhr.responseText)
         case _ => Left( log(xhr.statusText, xhr.status) )
       }
     }.recover { case error => Left( log(error) ) }
