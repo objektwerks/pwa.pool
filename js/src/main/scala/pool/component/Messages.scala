43bd62543f4206375f844a0e385e24225d93c37d
objektwerks <objektwerks@runbox.com>
2021-07-28 07:15:28 -0400

    edited messages

--- js/src/main/scala/pool/component/Messages.scala
+++ js/src/main/scala/pool/component/Messages.scala
@@ -5,7 +5,7 @@ import com.raquo.laminar.api.L._
 object Messages {
   def apply(messages: EventBus[String]): Div =
     div(
-      cls("w3-border-white w3-indigo"),
+      cls("w3-border-white w3-yellow w3-text-indigo"),
       child.text <-- messages.events
     )
 }
