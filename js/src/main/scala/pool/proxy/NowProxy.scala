8765ffa1fcd8e3ac0eb7e80e5a47ddd98af17930
objektwerks <objektwerks@runbox.com>
2021-08-03 07:03:03 -0400

    strip single quotes from now response

--- js/src/main/scala/pool/proxy/NowProxy.scala
+++ js/src/main/scala/pool/proxy/NowProxy.scala
@@ -9,7 +9,7 @@ object NowProxy extends Proxy {
   def post(url: String): Future[String] =
     Ajax.post(url = url, headers = headers).map { xhr =>
       xhr.status match {
-        case 200 => xhr.responseText
+        case 200 => xhr.responseText.stripPrefix("\"").stripSuffix("\"")
         case _ => xhr.statusText
       }
     }.recover { case error => error.getMessage }
