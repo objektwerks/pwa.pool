84439461a8edd940729260c9e211b3746e841e08
objektwerks <objektwerks@runbox.com>
2021-09-16 07:47:55 -0400

    edited list view

--- js/src/main/scala/pool/view/ListView.scala
+++ js/src/main/scala/pool/view/ListView.scala
@@ -3,9 +3,9 @@ package pool.view
 import com.raquo.laminar.api.L._
 
 object ListView {
-  def apply(entities: EventStream[Seq[Div]]): Div =
+  def apply(entityStream: EventStream[Seq[Div]]): Div =
     div(
       cls("w3-margin"),
-      children <-- entities
+      children <-- entityStream
     )
 }
