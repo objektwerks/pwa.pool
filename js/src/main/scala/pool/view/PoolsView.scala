ac4b9aa350ffaec6e5077a1a2e18b37f7712d1e7
objektwerks <objektwerks@runbox.com>
2021-09-18 08:28:30 -0400

    refactored pool signal and list view stream

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -13,8 +13,8 @@ import pool.text.{Errors, Header}
 object PoolsView {
   val id = getClass.getSimpleName
   val errors = new EventBus[String]
-  var poolSignal = Signal.fromValue(Seq.empty[Pool])
-  var poolStream = EventStream.fromValue(Seq.empty[Div])
+  var poolSignal: Signal[Seq[Pool]] = Signal.fromValue(Seq.empty[Pool])
+  var listViewSignal: Signal[Seq[Div ]] = Signal.fromValue(Seq.empty[Div])
 
   def init(context: Context): Unit = {
     val license = License(AccountDialog.account.now().license)
@@ -27,22 +27,21 @@ object PoolsView {
       child.text <-- pool.map(_.name)
     )
 
-  def handler(context: Context, errors: EventBus[String], state: State): Unit = {
+  def handler(context: Context, errors: EventBus[String], state: State): Unit =
     state match {
       case pools: Pools =>
         poolSignal = Signal.fromValue(pools.pools)
-        poolStream = poolSignal.split(_.id)((_, _, pool) => renderer(pool) ).changes
+        listViewSignal = poolSignal.split(_.id)((_, _, pool) => renderer(pool) )
       case id: Id => context.log(s"Pool id: $id for add pool.")
       case count: Count => context.log(s"Pool count: $count for update pool.")
       case _ => errors.emit(s"Invalid: $state")
     }
-  }
 
-  def apply(context: Context): Div = {
+  def apply(context: Context): Div =
     Container(id = id, isDisplayed = "none",
       Header("Pools"),
       Errors(errors),
-      ListView(poolStream),
+      ListView(listViewSignal),
       MenuButtonBar(
         MenuButton(name = "Add").amend {
           onClick --> { _ =>
@@ -56,5 +55,4 @@ object PoolsView {
         }
       )
     )
-  }
 }
