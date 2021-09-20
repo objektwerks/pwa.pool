2e7964e55b75dc1a07a17ccd2d43f3bf52419cf5
objektwerks <objektwerks@runbox.com>
2021-09-20 06:33:36 -0400

    enhanced pool dialog

--- js/src/main/scala/pool/dialog/PoolsDialog.scala
+++ js/src/main/scala/pool/dialog/PoolsDialog.scala
@@ -2,9 +2,9 @@ package pool.dialog
 
 import com.raquo.laminar.api.L._
 
-import java.time.LocalDate
-
-import pool.text.{Errors, Header}
+import pool.container.Field
+import pool.dialog.AccountDialog.account
+import pool.text.{Errors, Header, Label, Text}
 import pool.{Context, Pool}
 
 object PoolsDialog {
@@ -12,16 +12,28 @@ object PoolsDialog {
   val errors = new EventBus[String]
 
   val emptyPool = Pool(
-    license = AccountDialog.account.now().license,
+    license = "",
     name = "pool",
-    built = LocalDate.now().getYear,
+    built = 0,
     lat = 0,
     lon = 0,
     volume = 1000)
 
-  def apply(context: Context): Div =
+  def apply(context: Context, pool: Var[Pool]): Div =
     Modal(id = id,
       Header("Pool"),
-      Errors(errors)
+      Errors(errors),
+      Field(
+        Label(column = "25%", name = "License:"),
+        Text(column = "75%", Text.field(typeOf = "text", isReadOnly = true).amend {
+          value <-- account.signal.map(_.license)
+        })
+      ),
+      Field(
+        Label(column = "15%", name = "Name:"),
+        Text(column = "85%", Text.field(typeOf = "text").amend {
+          onInput.mapToValue.filter(_.nonEmpty) --> { name => pool.update( pool => pool.copy(name = name) ) }
+        })
+      )
     )
 }
