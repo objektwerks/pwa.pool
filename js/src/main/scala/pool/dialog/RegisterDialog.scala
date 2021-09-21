219ec574428f48179d141e55ad70729c298389b5
objektwerks <objektwerks@runbox.com>
2021-09-21 10:36:53 -0400

    refactored register dialog due to context entity move

--- js/src/main/scala/pool/dialog/RegisterDialog.scala
+++ js/src/main/scala/pool/dialog/RegisterDialog.scala
@@ -12,7 +12,6 @@ import pool.text._
 object RegisterDialog {
   val id = getClass.getSimpleName
   val errors = new EventBus[String]
-  val email = Var("")
 
   def handler(context: Context, errors: EventBus[String], event: Event): Unit =
     event match {
@@ -30,7 +29,7 @@ object RegisterDialog {
       Field(
         Label(column = "15%", name = "Email:"),
         Text(column = "85%", Text.field(typeOf = "email").amend {
-          onInput.mapToValue.filter(_.nonEmpty) --> email
+          onInput.mapToValue.filter(_.nonEmpty) --> context.email
         })
       ),
       MenuButtonBar(
@@ -39,7 +38,7 @@ object RegisterDialog {
         },
         MenuButton(name = "Register").amend {
           onClick --> { _ =>
-            val command = Register(email.now())
+            val command = Register(context.email.now())
             val response = CommandProxy.post(context.registerUrl, Account.emptyLicense, command)
             EventHandler.handle(context, errors, response, handler)
           }
