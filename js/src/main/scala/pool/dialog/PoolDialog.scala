74d5e6c68fd575eb833ad6283a2389d98876f0f4
objektwerks <objektwerks@runbox.com>
2021-09-21 10:49:46 -0400

    removed pool var from pool dialog apply method

--- js/src/main/scala/pool/dialog/PoolDialog.scala
+++ js/src/main/scala/pool/dialog/PoolDialog.scala
@@ -5,25 +5,15 @@ import com.raquo.laminar.api.L._
 import pool.container.Field
 import pool.menu.{MenuButton, MenuButtonBar}
 import pool.text.{Errors, Header, Label, Text}
-import pool.{Context, Pool}
+import pool.Context
 
 object PoolDialog {
   val id = getClass.getSimpleName
   val addButtonId = id + "-add-button"
   val updateButtonId = id + "-update-button"
   val errors = new EventBus[String]
-  val emptyPool = Var(
-    Pool(
-      license = "",
-      name = "pool",
-      built = 0,
-      lat = 0.0,
-      lon = 0.0,
-      volume = 1000
-    )
-  )
 
-  def apply(context: Context, pool: Var[Pool]): Div =
+  def apply(context: Context): Div =
     Modal(id = id,
       Header("Pool"),
       Errors(errors),
@@ -36,45 +26,45 @@ object PoolDialog {
       Field(
         Label(column = "25%", name = "Name:"),
         Text(column = "75%", Text.field(typeOf = "text").amend {
-          value <-- pool.signal.map(_.name)
+          value <-- context.pool.signal.map(_.name)
           onInput.mapToValue.filter(_.nonEmpty) --> { name =>
-            pool.update( pool => pool.copy(name = name) )
+            context.pool.update( pool => pool.copy(name = name) )
           }
         })
       ),
       Field(
         Label(column = "25%", name = "Built:"),
         Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- pool.signal.map(_.built.toString)
+          value <-- context.pool.signal.map(_.built.toString)
           onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> { built =>
-            pool.update( pool => pool.copy(built = built) )
+            context.pool.update( pool => pool.copy(built = built) )
           }
         })
       ),
       Field(
         Label(column = "25%", name = "Lat:"),
         Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- pool.signal.map(_.lat.toString)
+          value <-- context.pool.signal.map(_.lat.toString)
           onInput.mapToValue.filter(_.toDoubleOption.nonEmpty).map(_.toDouble) --> { lat =>
-            pool.update( pool => pool.copy(lat = lat) )
+            context.pool.update( pool => pool.copy(lat = lat) )
           }
         })
       ),
       Field(
         Label(column = "25%", name = "Lon:"),
         Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- pool.signal.map(_.lon.toString)
+          value <-- context.pool.signal.map(_.lon.toString)
           onInput.mapToValue.filter(_.toDoubleOption.nonEmpty).map(_.toDouble) --> { lon =>
-            pool.update( pool => pool.copy(lon = lon) )
+            context.pool.update( pool => pool.copy(lon = lon) )
           }
         })
       ),
       Field(
         Label(column = "25%", name = "Volume:"),
         Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- pool.signal.map(_.volume.toString)
+          value <-- context.pool.signal.map(_.volume.toString)
           onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> { volume =>
-            pool.update( pool => pool.copy(volume = volume) )
+            context.pool.update( pool => pool.copy(volume = volume) )
           }
         })
       ),
