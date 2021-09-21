538c829013df922cee8fe239aaa0ffeb2d251afe
objektwerks <objektwerks@runbox.com>
2021-09-21 10:34:52 -0400

    refactored list view due to context entity move

--- js/src/main/scala/pool/view/ListView.scala
+++ js/src/main/scala/pool/view/ListView.scala
@@ -3,12 +3,12 @@ package pool.view
 import com.raquo.laminar.api.L._
 
 object ListView {
-  def apply(itemsSignal: Signal[Seq[Li]]): Div =
+  def apply(listItems: Signal[Seq[Li]]): Div =
     div(
       cls("w3-container w3-margin"),
       ul(
         cls("w3-ul w3-hoverable"),
-        children <-- itemsSignal
+        children <-- listItems
       )
     )
 }
