bf1cc78262c3a16ed2bab2d1674ffdbdd7b99c95
objektwerks <objektwerks@runbox.com>
2021-09-30 10:39:45 -0400

    edited list view

--- js/src/main/scala/pool/view/ListView.scala
+++ js/src/main/scala/pool/view/ListView.scala
@@ -5,7 +5,7 @@ import com.raquo.laminar.api.L._
 object ListView {
   def apply(listItems: Signal[Seq[Li]]): Div =
     div(
-      cls("w3-container w3-margin"),
+      cls("w3-container"),
       ul(
         cls("w3-ul w3-hoverable"),
         children <-- listItems
