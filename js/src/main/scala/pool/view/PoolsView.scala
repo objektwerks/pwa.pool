529f435a9bdee83e1809ff5922ff6af3a3a0f7e2
objektwerks <objektwerks@runbox.com>
2021-10-01 03:14:07 -0400

    refactored pools view

--- js/src/main/scala/pool/view/PoolsView.scala
+++ js/src/main/scala/pool/view/PoolsView.scala
@@ -14,26 +14,21 @@ object PoolsView {
   val errors = new EventBus[String]
   var listItems = Var(Seq.empty[Li]).toObservable
 
+  def handler(context: Context, errors: EventBus[String], state: State): Unit =
+    state match {
+      case pools: Pools =>
+        context.pools.set(pools.pools)
+        listItems = context.pools.signal.split(_.id)((_, _, pool) => ListView.renderItem(pool.map(_.name)))
+      case id: Id => context.log(s"Pool id: $id for add pool.")
+      case count: Count => context.log(s"Pool count: $count for update pool.")
+      case _ => errors.emit(s"Invalid: $state")
+    }
+
   def init(context: Context): Unit = {
     val license = License(context.account.now().license)
     val response = EntityProxy.post(context.poolsUrl, license.key, license)
     StateHandler.handle(context, errors, response, handler)
   }
-
-  def renderer(pool: Signal[Pool]): Li =
-    li(
-      child.text <-- pool.map(_.name)
-    )
-
-  def handler(context: Context, errors: EventBus[String], state: State): Unit =
-    state match {
-      case pools: Pools =>
-        context.pools.set(pools.pools)
-        listItems = context.pools.signal.split(_.id)((_, _, pool) => renderer(pool) )
-      case id: Id => context.log(s"Pool id: $id for add pool.")
-      case count: Count => context.log(s"Pool count: $count for update pool.")
-      case _ => errors.emit(s"Invalid: $state")
-    }
 
   def apply(context: Context): Div =
     Container(id = id, isDisplayed = "none",
