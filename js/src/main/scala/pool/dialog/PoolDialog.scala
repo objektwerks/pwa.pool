d43d330036d071ff8a1cd4b24aee5bb2f40c61a9
objektwerks <objektwerks@runbox.com>
2021-10-01 08:17:07 -0400

    implemented pool dialog menu buttons

--- js/src/main/scala/pool/dialog/PoolDialog.scala
+++ js/src/main/scala/pool/dialog/PoolDialog.scala
@@ -2,10 +2,10 @@ package pool.dialog
 
 import com.raquo.laminar.api.L._
 
+import pool.{Account, Context, Pool}
 import pool.container.Field
 import pool.menu.{MenuButton, MenuButtonBar}
 import pool.text.{Errors, Header, Label, Text}
-import pool.Context
 
 object PoolDialog {
   val id = getClass.getSimpleName
@@ -13,20 +13,23 @@ object PoolDialog {
   val updateButtonId = id + "-update-button"
   val errors = new EventBus[String]
 
-  def apply(context: Context): Div =
+  def apply(context: Context,
+            account: Var[Account],
+            pool: Var[Pool],
+            readOnly: Boolean = false): Div =
     Modal(id = id,
       Header("Pool"),
       Errors(errors),
       Field(
         Label(column = "25%", name = "License:"),
         Text(column = "75%", Text.field(typeOf = "text", isReadOnly = true).amend {
-          value <-- context.account.signal.map(_.license)
+          value <-- account.signal.map(_.license)
         })
       ),
       Field(
         Label(column = "25%", name = "Name:"),
-        Text(column = "75%", Text.field(typeOf = "text").amend {
-          value <-- context.pool.signal.map(_.name)
+        Text(column = "75%", Text.field(typeOf = "text", isReadOnly = readOnly).amend {
+          value <-- pool.signal.map(_.name)
           onInput.mapToValue.filter(_.nonEmpty) --> { name =>
             context.pool.update( pool => pool.copy(name = name) )
           }
@@ -34,8 +37,8 @@ object PoolDialog {
       ),
       Field(
         Label(column = "25%", name = "Built:"),
-        Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- context.pool.signal.map(_.built.toString)
+        Text(column = "75%", Text.field(typeOf = "number", isReadOnly = readOnly).amend {
+          value <-- pool.signal.map(_.built.toString)
           onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> { built =>
             context.pool.update( pool => pool.copy(built = built) )
           }
@@ -43,8 +46,8 @@ object PoolDialog {
       ),
       Field(
         Label(column = "25%", name = "Lat:"),
-        Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- context.pool.signal.map(_.lat.toString)
+        Text(column = "75%", Text.field(typeOf = "number", isReadOnly = readOnly).amend {
+          value <-- pool.signal.map(_.lat.toString)
           onInput.mapToValue.filter(_.toDoubleOption.nonEmpty).map(_.toDouble) --> { lat =>
             context.pool.update( pool => pool.copy(lat = lat) )
           }
@@ -52,8 +55,8 @@ object PoolDialog {
       ),
       Field(
         Label(column = "25%", name = "Lon:"),
-        Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- context.pool.signal.map(_.lon.toString)
+        Text(column = "75%", Text.field(typeOf = "number", isReadOnly = readOnly).amend {
+          value <-- pool.signal.map(_.lon.toString)
           onInput.mapToValue.filter(_.toDoubleOption.nonEmpty).map(_.toDouble) --> { lon =>
             context.pool.update( pool => pool.copy(lon = lon) )
           }
@@ -61,26 +64,22 @@ object PoolDialog {
       ),
       Field(
         Label(column = "25%", name = "Volume:"),
-        Text(column = "75%", Text.field(typeOf = "number").amend {
-          value <-- context.pool.signal.map(_.volume.toString)
+        Text(column = "75%", Text.field(typeOf = "number", isReadOnly = readOnly).amend {
+          value <-- pool.signal.map(_.volume.toString)
           onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> { volume =>
             context.pool.update( pool => pool.copy(volume = volume) )
           }
         })
       ),
       MenuButtonBar(
-        MenuButton(name = "Cancel").amend {
+        MenuButton(name = "Close").amend {
           onClick --> { _ => context.hide(id) }
         },
         MenuButton(id = addButtonId, name = "Add").amend {
-          onClick --> { _ =>
-            context.log("add pool")
-          }
+          onClick --> { _ => context.hide(id) }
         },
         MenuButton(id = updateButtonId, name = "Update").amend {
-          onClick --> { _ =>
-            context.log("update pool")
-          }
+          onClick --> { _ => context.hide(id) }
         }
       )
     )
