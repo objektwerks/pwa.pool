aac8483b5bac19ac9433579e993eeffa16bc13db
objektwerks <objektwerks@runbox.com>
2021-10-01 03:13:22 -0400

    added render item method to list view

--- js/src/main/scala/pool/view/ListView.scala
+++ js/src/main/scala/pool/view/ListView.scala
@@ -3,12 +3,17 @@ package pool.view
 import com.raquo.laminar.api.L._
 
 object ListView {
-  def apply(listItems: Signal[Seq[Li]]): Div =
+  def apply(items: Signal[Seq[Li]]): Div =
     div(
       cls("w3-container"),
       ul(
         cls("w3-ul w3-hoverable"),
-        children <-- listItems
+        children <-- items
       )
+    )
+
+  def renderItem(item: Signal[String]): Li =
+    li(
+      child.text <-- item
     )
 }
