28da0dea544f2002d382fb49f29aac50a46d7a57
objektwerks <objektwerks@runbox.com>
2021-10-04 02:12:06 -0400

    refactored client

--- js/src/main/scala/pool/Client.scala
+++ js/src/main/scala/pool/Client.scala
@@ -1,12 +1,10 @@
 package pool
 
 import com.raquo.laminar.api.L._
+
 import org.scalajs.dom._
-import pool.container.Container
-import pool.dialog.{AccountDialog, LoginDialog, PoolDialog, RegisterDialog}
-import pool.menu.HomeMenu
+
 import pool.proxy.NowProxy
-import pool.view.PoolsView
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
@@ -14,23 +12,11 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("Client")
 class Client(publicUrl: String, apiUrl: String) extends js.Object {
-  val content = Container()
-  val context = Context(content, publicUrl, apiUrl)
-  context.log(s"[context] public url: $publicUrl api url: $apiUrl")
-
-  content.amend {
-    HomeMenu(
-      context,
-      RegisterDialog(context),
-      LoginDialog(context),
-      AccountDialog(context)
-    )
-    PoolsView(context, PoolDialog(context))
-  }
-
-  ServiceWorker.register()
+  val context = Context(publicUrl, apiUrl)
 
   NowProxy.post(s"$publicUrl/now").foreach(now => context.log(s"[now] $now"))
 
-  render(document.getElementById("content"), content)
+  ServiceWorker.register()
+
+  render(document.getElementById("content"), context.content)
 }
