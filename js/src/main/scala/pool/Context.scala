f80104b44c1874ae9e1ccc859fd26433f48f6359
objektwerks <objektwerks@runbox.com>
2021-10-04 02:11:45 -0400

    refactored context

--- js/src/main/scala/pool/Context.scala
+++ js/src/main/scala/pool/Context.scala
@@ -4,11 +4,30 @@ import com.raquo.laminar.api.L._
 
 import org.scalajs.dom.{console, document}
 
+import pool.container.Container
+import pool.dialog.{AccountDialog, LoginDialog, PoolDialog, RegisterDialog}
+import pool.menu.HomeMenu
+import pool.view.PoolsView
+
 object Context {
+  def apply(publicUrl: String, apiUrl: String): Context = new Context(publicUrl, apiUrl)
+
   def log(message: Any): Unit = console.log(message)
 }
 
-case class Context(content: Div, publicUrl: String, apiUrl: String) extends Product with Serializable {
+class Context(publicUrl: String, apiUrl: String) {
+  log(s"[context] public url: $publicUrl api url: $apiUrl")
+
+  val content = Container(
+    HomeMenu(
+      this,
+      RegisterDialog(this),
+      LoginDialog(this),
+      AccountDialog(this)
+    ),
+    PoolsView(this, PoolDialog(this))
+  )
+
   val email = Var("")
   val pin = Var(0)
   val account = Var(Account.emptyAccount)
