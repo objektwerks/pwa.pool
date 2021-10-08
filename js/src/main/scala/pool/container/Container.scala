bbf98085ea9f15382573546e8dd9f7d73cec8c24
objektwerks <objektwerks@runbox.com>
2021-10-08 01:48:02 -0400

    reformatted container

--- js/src/main/scala/pool/container/Container.scala
+++ js/src/main/scala/pool/container/Container.scala
@@ -5,8 +5,7 @@ import com.raquo.laminar.api.L._
 object Container {
   def apply(divs: Div*): Div = div(cls("w3-container"), divs )
 
-  def apply(id: String,
-            divs: Div*): Div = div(idAttr(id), cls("w3-container"), divs )
+  def apply(id: String, divs: Div*): Div = div(idAttr(id), cls("w3-container"), divs )
 
   def apply(id: String,
             isDisplayed: String,
