8dbec296f5249abf13a96a624c1f145979ba6e1a
objektwerks <objektwerks@runbox.com>
2021-10-08 01:49:29 -0400

    added text, integer and double input types to text

--- js/src/main/scala/pool/text/Text.scala
+++ js/src/main/scala/pool/text/Text.scala
@@ -3,20 +3,45 @@ package pool.text
 import com.raquo.laminar.api.L._
 
 object Text {
-  def apply(column: String,
-            input: Input): Div =
+  def apply(column: String, input: Input): Div =
     div(
       cls("w3-col"),
       width(column),
       input
     )
 
-  def field(typeOf: String,
-            isReadOnly: Boolean = false): Input =
+  def field(typeOf: String, isReadOnly: Boolean = false): Input =
     input(
       cls("w3-input w3-hover-light-gray w3-text-indigo"),
       typ(typeOf),
       required(true),
       readOnly(isReadOnly)
+    )
+
+  def text(typeOf: String, text: Var[String], isReadOnly: Boolean = false): Input =
+    input(
+      cls("w3-input w3-hover-light-gray w3-text-indigo"),
+      typ(typeOf),
+      required(true),
+      readOnly(isReadOnly),
+      onInput.mapToValue.filter(_.nonEmpty) --> text
+    )
+
+  def integer(integer: Var[Int], isReadOnly: Boolean = false): Input =
+    input(
+      cls("w3-input w3-hover-light-gray w3-text-indigo"),
+      typ("number"),
+      required(true),
+      readOnly(isReadOnly),
+      onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> integer
+    )
+
+  def double(double: Var[Double], isReadOnly: Boolean = false): Input =
+    input(
+      cls("w3-input w3-hover-light-gray w3-text-indigo"),
+      typ("number"),
+      required(true),
+      readOnly(isReadOnly),
+      onInput.mapToValue.filter(_.toDoubleOption.nonEmpty).map(_.toDouble) --> double
     )
 }
