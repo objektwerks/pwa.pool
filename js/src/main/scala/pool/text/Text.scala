0fe85fc47041aff71cc5dfa40cbab39a7d332aae
objektwerks <objektwerks@runbox.com>
2021-10-08 16:08:45 -0400

    refactored text component double validation

--- js/src/main/scala/pool/text/Text.scala
+++ js/src/main/scala/pool/text/Text.scala
@@ -49,6 +49,6 @@ object Text {
       typ("number"),
       required(true),
       readOnly(isReadOnly),
-      onInput.mapToValue.filter(_.toDoubleOption.nonEmpty).map(_.toDouble) --> double
+      onInput.mapToValue.filter(value => value.charAt(0).isDigit || value == ".") --> double
     )
 }
