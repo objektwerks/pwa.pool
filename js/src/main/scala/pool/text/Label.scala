7ffc52442b713e542d651d1ab979d01e56c51ffa
objektwerks <objektwerks@runbox.com>
2021-10-13 06:23:31 -0400

    removed apply from Label

--- js/src/main/scala/pool/text/Label.scala
+++ js/src/main/scala/pool/text/Label.scala
@@ -5,11 +5,4 @@ import com.raquo.laminar.api.L._
 object Label {
   def apply(name: String): Label =
     label(cls("w3-left-align w3-text-indigo"), name)
-
-/*  def apply(column: String, name: String): Div =
-    div(
-      cls("w3-col"),
-      width(column),
-      label(cls("w3-left-align w3-text-indigo"), name)
-    )*/
 }
