74055fe1e0e103ec12ef4a820182836b99ceda38
objektwerks <objektwerks@runbox.com>
2021-10-13 07:00:23 -0400

    reformatted code

--- js/src/main/scala/pool/text/Label.scala
+++ js/src/main/scala/pool/text/Label.scala
@@ -3,6 +3,5 @@ package pool.text
 import com.raquo.laminar.api.L._
 
 object Label {
-  def apply(name: String): Label =
-    label(cls("w3-left-align w3-text-indigo"), name)
+  def apply(name: String): Label = label(cls("w3-left-align w3-text-indigo"), name)
 }
