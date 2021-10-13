de98660010e2a6c5a357a964f27892b60dba44a1
objektwerks <objektwerks@runbox.com>
2021-10-13 06:24:32 -0400

    refactored login dialog to use refactored field, label and text

--- js/src/main/scala/pool/dialog/LoginDialog.scala
+++ js/src/main/scala/pool/dialog/LoginDialog.scala
@@ -31,8 +31,8 @@ object LoginDialog {
       Header("Login"),
       Errors(errors),
       Field(
-        Label(column = "15%", name = "Pin:"),
-        Text(column = "85%", Text.integer(integer = context.pin))
+        Label(name = "Pin:"),
+        Text.integer(integer = context.pin)
       ),
       MenuButtonBar(
         MenuButton(name = "Cancel").amend {
