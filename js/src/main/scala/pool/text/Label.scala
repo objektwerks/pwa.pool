52a2fb36311e7b4e9252bb068ccbd5ea488f5c1f
objektwerks <objektwerks@runbox.com>
2021-10-13 06:21:56 -0400

    added apply to Label

--- js/src/main/scala/pool/text/Label.scala
+++ js/src/main/scala/pool/text/Label.scala
@@ -3,10 +3,13 @@ package pool.text
 import com.raquo.laminar.api.L._
 
 object Label {
-  def apply(column: String, name: String): Div =
+  def apply(name: String): Label =
+    label(cls("w3-left-align w3-text-indigo"), name)
+
+/*  def apply(column: String, name: String): Div =
     div(
       cls("w3-col"),
       width(column),
       label(cls("w3-left-align w3-text-indigo"), name)
-    )
+    )*/
 }
