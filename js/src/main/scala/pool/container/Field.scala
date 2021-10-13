5cb193313f77a530de2b3665decec3b2f163d738
objektwerks <objektwerks@runbox.com>
2021-10-13 06:21:31 -0400

    add apply to Field

--- js/src/main/scala/pool/container/Field.scala
+++ js/src/main/scala/pool/container/Field.scala
@@ -3,9 +3,15 @@ package pool.container
 import com.raquo.laminar.api.L._
 
 object Field {
-  def apply(label: Div, input: Div): Div =
+  def apply(label: Label, input: Input): Div =
     div(cls("w3-row w3-margin"),
       label,
       input
     )
+
+/*  def apply(label: Div, input: Div): Div =
+    div(cls("w3-row w3-margin"),
+      label,
+      input
+    )*/
 }
