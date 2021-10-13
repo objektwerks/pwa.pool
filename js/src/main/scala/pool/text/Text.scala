1d3f45ce7a1978d842c4e4d653bcf83b7d5851ab
objektwerks <objektwerks@runbox.com>
2021-10-13 06:22:27 -0400

    removed apply from Text

--- js/src/main/scala/pool/text/Text.scala
+++ js/src/main/scala/pool/text/Text.scala
@@ -3,13 +3,6 @@ package pool.text
 import com.raquo.laminar.api.L._
 
 object Text {
-  def apply(column: String, input: Input): Div =
-    div(
-      cls("w3-col"),
-      width(column),
-      input
-    )
-
   def field(typeOf: String): Input =
     input(
       cls("w3-input w3-hover-light-gray w3-text-indigo"),
