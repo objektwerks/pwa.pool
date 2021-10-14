40c7e9f813497d6b8ed2bb332cd1ca33ff3b695f
objektwerks <objektwerks@runbox.com>
2021-10-14 07:58:13 -0400

    added email to Login

--- shared/src/main/scala/pool/Command.scala
+++ shared/src/main/scala/pool/Command.scala
@@ -4,7 +4,7 @@ sealed trait Command extends Product with Serializable
 
 final case class Register(email: String) extends Command
 
-final case class Login(pin: String) extends Command
+final case class Login(email: String, pin: String) extends Command
 
 final case class Deactivate(license: String) extends Command
 
