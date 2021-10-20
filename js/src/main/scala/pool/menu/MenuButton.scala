a37d8c270e8671bdd488f296523d643a01d7dd98
objektwerks <objektwerks@runbox.com>
2021-10-20 07:48:55 -0400

    edited css

--- js/src/main/scala/pool/menu/MenuButton.scala
+++ js/src/main/scala/pool/menu/MenuButton.scala
@@ -3,7 +3,7 @@ package pool.menu
 import com.raquo.laminar.api.L._
 
 object MenuButton {
-  val css = "w3-bar-item w3-button w3-margin w3-text-indigo"
+  val css = "w3-bar-item w3-button w3-text-indigo"
 
   def apply(name: String): Button = button(cls(css), name)
 
