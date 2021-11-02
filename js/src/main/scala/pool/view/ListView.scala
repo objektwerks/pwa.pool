81a70ea80e39fd41781f4a754d59cff304156581
objektwerks <objektwerks@runbox.com>
2021-11-02 08:29:09 -0400

    renamed method args in list view

--- js/src/main/scala/pool/view/ListView.scala
+++ js/src/main/scala/pool/view/ListView.scala
@@ -3,18 +3,18 @@ package pool.view
 import com.raquo.laminar.api.L._
 
 object ListView {
-  def apply(items: Signal[Seq[Li]]): Div =
+  def apply(itemsSignal: Signal[Seq[Li]]): Div =
     div(
       cls("w3-container"),
       ul(
         cls("w3-ul w3-hoverable"),
-        children <-- items
+        children <-- itemsSignal
       )
     )
 
-  def renderItem(item: Signal[String]): Li =
+  def renderItem(itemSignal: Signal[String]): Li =
     li(
       cls("w3-text-indigo w3-display-container"),
-      child.text <-- item
+      child.text <-- itemSignal
     )
 }
