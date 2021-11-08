c0769d33889d1e9682386eb8ea1d894b1c2fa2ef
objektwerks <objektwerks@runbox.com>
2021-11-08 12:40:51 -0500

    added send / fetch method to now proxy

--- js/src/main/scala/pool/proxy/NowProxy.scala
+++ js/src/main/scala/pool/proxy/NowProxy.scala
@@ -2,9 +2,11 @@ package pool.proxy
 
 import org.scalajs.dom.ext.Ajax
 import org.scalajs.dom.ext.AjaxException
+import org.scalajs.dom._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.scalajs.js.Thenable.Implicits._
 
 object NowProxy extends Proxy {
   def post(url: String): Future[String] =
@@ -14,4 +16,14 @@ object NowProxy extends Proxy {
         case _ => xhr.statusText
       }
     }.recover { case AjaxException(xhr) => xhr.statusText }
+
+  def send(url: String): Future[String] = {
+    ( for {
+      response <- fetch(url)
+      json     <- response.text()
+      text     = json.stripPrefix("\"").stripSuffix("\"")
+    } yield {
+      text
+    } ).recover { case error: Exception => error.getMessage }
+  }
 }
