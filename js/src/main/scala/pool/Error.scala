b516bb45a52cc3a39389ee7c462f36ff606fa927
objektwerks <objektwerks@runbox.com>
2022-03-19 11:17:53 -0400

    clean js

--- js/src/main/scala/pool/Error.scala
+++
@@ -1,9 +0,0 @@
-package pool
-
-object Error:
-  val emailAddressError = "Enter a valid email address."
-  val pinError = "Enter a valid pin of 6 alphanumeric characters."
-
-  val nameError = "Enter a 2 characters or more."
-  val builtError = "Enter a year greater than 1900."
-  val volumeError = "Enter a volume greater than 999."
--- js/src/main/scala/pool/Message.scala
+++
@@ -1,5 +0,0 @@
-package pool
-
-object Message:
-  val enterMessage = "Record your pin!"
-  val pinMessage = "Keep your pin of 6 alphanumeric characters in a secure place!"
