8db50628389ffd939924dbd51420f90b71d06459
objektwerks <objektwerks@runbox.com>
2022-03-21 08:47:07 -0400

    refactored authorizer

--- jvm/src/main/scala/pool/Authorizer.scala
+++ jvm/src/main/scala/pool/Authorizer.scala
@@ -4,4 +4,4 @@ final class Authorizer(service: Service):
   def authorize(command: Command): Event =
     command match
       case license: License => service.authorize(license.license)
-      case _ => Authorized("")
+      case Register(_) | Login(_, _) => Authorized("")
