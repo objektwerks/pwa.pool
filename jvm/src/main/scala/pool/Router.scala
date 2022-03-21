e518b1cd5a2e6995177292decdc150a3ec48346a
objektwerks <objektwerks@runbox.com>
2022-03-21 08:46:09 -0400

    enhanced router

--- jvm/src/main/scala/pool/Router.scala
+++ jvm/src/main/scala/pool/Router.scala
@@ -22,6 +22,7 @@ final class Router(dispatcher: Dispatcher, store: Store) extends Routes with Laz
     val event = dispatcher.dispatch(command)
     logger.debug(s"*** Event: $event")
     event match {
+      case unauthorized: Unauthorized => store.addFault( Fault(unauthorized.license) )
       case fault: Fault => store.addFault(fault)
       case _ =>
     }
