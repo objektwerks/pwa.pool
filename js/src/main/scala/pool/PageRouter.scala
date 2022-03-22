ce9bb5d5abfd7cd99ea0a959e541ad717ac2dc49
objektwerks <objektwerks@runbox.com>
2022-03-22 05:32:07 -0400

    corrected splitter error

--- js/src/main/scala/pool/PageRouter.scala
+++ js/src/main/scala/pool/PageRouter.scala
@@ -40,7 +40,7 @@ object PageRouter:
   )
 
   val splitter = SplitRender[Page, HtmlElement](router.$currentPage)
-    .collectStatic(IndexPage) { IndexView() }
+    .collectStatic(RootPage) { IndexView() }
     .collectStatic(RegisterPage) { RegisterView(Model.emailAddressVar, Model.pinVar, Model.accountVar) }
     .collectStatic(LoginPage) { LoginView(Model.emailAddressVar, Model.pinVar, Model.accountVar) }
     .collectStatic(HomePage) { HomeView(Model.accountVar) }
