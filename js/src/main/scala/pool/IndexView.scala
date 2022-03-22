d2777c5deb65c6069b39f493f2ec42cb8b32a36c
objektwerks <objektwerks@runbox.com>
2022-03-22 05:24:10 -0400

    reformatted index view

--- js/src/main/scala/pool/IndexView.scala
+++ js/src/main/scala/pool/IndexView.scala
@@ -7,20 +7,20 @@ import org.scalajs.dom.console.log
 import Components.*
 
 object IndexView extends View:
-    def apply(): HtmlElement =
-      div(
-        bar(
-          btn("Login").amend {
-            onClick --> { _ =>
-              log("Index -> Login menu item onClick")
-              route(LoginPage)
-            }
-          },
-          rbtn("Register").amend {
-            onClick --> { _ =>
-              log("Index -> Register menu item onClick")
-              route(RegisterPage)
-            }
-          }          
-        )
+  def apply(): HtmlElement =
+    div(
+      bar(
+        btn("Login").amend {
+          onClick --> { _ =>
+            log("Index -> Login menu item onClick")
+            route(LoginPage)
+          }
+        },
+        rbtn("Register").amend {
+          onClick --> { _ =>
+            log("Index -> Register menu item onClick")
+            route(RegisterPage)
+          }
+        }          
       )
+    )
