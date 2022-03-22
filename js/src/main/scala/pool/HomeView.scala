5287417a8579ecf8faa4e7e7ef21e5c576c80032
objektwerks <objektwerks@runbox.com>
2022-03-22 05:23:49 -0400

    reformatted home view

--- js/src/main/scala/pool/HomeView.scala
+++ js/src/main/scala/pool/HomeView.scala
@@ -8,21 +8,21 @@ import Components.*
 import Validators.*
 
 object HomeView extends View:
-    def apply(accountVar: Var[Account]): HtmlElement =
-      div(
-        bar(
-          btn("Account").amend {
-            onClick --> { _ =>
-              log("Home -> Account menu item onClick")
-              route(AccountPage)
-            }
-          },
-          rbtn("Pools").amend {
-            disabled <-- accountVar.signal.map { account => account.isDeactivated }
-            onClick --> { _ =>
-              log("Home -> Pools menu item onClick")
-              route(PoolsPage)
-            }
-          }          
-        )
+  def apply(accountVar: Var[Account]): HtmlElement =
+    div(
+      bar(
+        btn("Account").amend {
+          onClick --> { _ =>
+            log("Home -> Account menu item onClick")
+            route(AccountPage)
+          }
+        },
+        rbtn("Pools").amend {
+          disabled <-- accountVar.signal.map { account => account.isDeactivated }
+          onClick --> { _ =>
+            log("Home -> Pools menu item onClick")
+            route(PoolsPage)
+          }
+        }          
       )
+    )
