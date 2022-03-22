dcf28b6a914a58fcaea6484fdc1372f2b8695b1a
objektwerks <objektwerks@runbox.com>
2022-03-22 05:33:08 -0400

    renamed index view to root view

--- js/src/main/scala/pool/RootView.scala
+++ js/src/main/scala/pool/RootView.scala
@@ -6,19 +6,19 @@ import org.scalajs.dom.console.log
 
 import Components.*
 
-object IndexView extends View:
+object RootView extends View:
   def apply(): HtmlElement =
     div(
       bar(
         btn("Login").amend {
           onClick --> { _ =>
-            log("Index -> Login menu item onClick")
+            log("Root -> Login menu item onClick")
             route(LoginPage)
           }
         },
         rbtn("Register").amend {
           onClick --> { _ =>
-            log("Index -> Register menu item onClick")
+            log("Root -> Register menu item onClick")
             route(RegisterPage)
           }
         }          
